0ded80c114fe7ce525e8f30fcb0d70470fafb72b
mikkelfo <mikk528a@gmail.com>
2022-10-14 16:27:49 +0200

    Fixed model initialization

--- model/model.py
+++ model/model.py
@@ -5,15 +5,16 @@ from embeddings import EhrEmbeddings
 
 class BertEHRModel(BertForPreTraining):
     def __init__(self, config):
-        super(self).__init__(config)
+        super(BertEHRModel, self).__init__(config)
         self.config = config
 
-        self.embeddings = EhrEmbeddings()
+        self.embeddings = EhrEmbeddings(config)
 
 
 if __name__ == '__main__':
     config = BertConfig(
-        vocab_size=None,              
-        max_position_embeddings=None, 
-        type_vocab_size=None
+        # vocab_size=None,              
+        # max_position_embeddings=None, 
+        # type_vocab_size=None
     )
+    model = BertEHRModel(config)

1dfa6f52d65a85dd1f28854033bd1fc6577f009c
Mikkelfo <mikk528a@gmail.com>
2023-02-16 12:34:52 +0100

    Updated format file loading

---
+++ data/creators.py
@@ -0,0 +1,98 @@
+import pandas as pd
+from datetime import datetime
+import glob
+
+class BaseCreator():
+    def __init__(self, config):
+        self.config: dict = config
+
+    def __call__(self, concepts=None):
+        return self.create(concepts)
+
+    def create(self):
+        raise NotImplementedError
+
+    def read_file(self, cfg, file_path) -> pd.DataFrame:
+        file_path = f'{cfg.data_dir}/{file_path}'
+
+        file_type = file_path.split(".")[-1]
+        if file_type == 'csv':
+            return pd.read_csv(file_path)
+        elif file_type == 'parquet':
+            return pd.read_parquet(file_path)
+
+class ConceptCreator(BaseCreator):
+    def create(self, concepts):
+        # Get all concept files
+        path = glob.glob('concept.*', root_dir=self.config.data_dir)
+
+        # Filter out concepts files
+        if self.config.get('concepts') is not None:
+            path = [p for p in path if p.split('.')[1] in self.config.concepts]
+        
+        # Load concepts
+        concepts = pd.concat([self.read_file(self.config, p) for p in path]).reset_index(drop=True)
+        
+        concepts = concepts.sort_values('TIMESTAMP')
+        concepts['TIMESTAMP'] = pd.to_datetime(concepts['TIMESTAMP'].str.slice(stop=10))
+
+        return concepts
+
+class AgeCreator(BaseCreator):
+    def create(self, concepts):
+        patients_info = self.read_file(self.config, 'patients_info.csv')
+        # Create PID -> BIRTHDATE dict
+        birthdates = pd.Series(patients_info['BIRTHDATE'].values, index=patients_info['PID']).to_dict()
+        # Calculate approximate age
+        ages = (concepts['TIMESTAMP'] - concepts['Key.Patient'].map(birthdates)).dt.days // 365.25
+
+        concepts['AGE'] = ages
+        return concepts
+
+class AbsposCreator(BaseCreator):
+    def create(self, concepts):
+        year, month, day = self.config.abspos['year'], self.config.abspos['month'], self.config.abspos['day']
+        origin_point = datetime(year, month, day)
+        abspos = (concepts['TIMESTAMP'] - origin_point).dt.days
+
+        concepts['ABSPOS'] = abspos
+        return concepts
+
+class SegmentCreator(BaseCreator):
+    def create(self, concepts):
+        concepts['ADMISSION_ID'] = self._infer_admission_id(concepts)
+
+        segments = concepts.groupby('PID')['ADMISSION_ID'].transform(lambda x: pd.factorize(x)[0]+1)
+
+        concepts['SEGMENT'] = segments
+        return concepts
+
+    def _infer_admission_id(self, df):
+        bf = df.sort_values('PID')
+        mask = bf['ADMISSION_ID'].fillna(method='ffill') != bf['ADMISSION_ID'].fillna(method='bfill')
+        bf.loc[mask, 'ADMISSION_ID'] = bf.loc[mask, 'ADMISSION_ID'].map(lambda x: 'unq_') + list(map(str, range(mask.sum())))
+        bf['ADMISSION_ID'] = bf['ADMISSION_ID'].fillna(method='ffill')
+        
+        return bf['ADMISSION_ID']
+
+class DemographicsCreator(BaseCreator):
+    def create(self, concepts):
+        patients_info = self.read_file(self.config, 'patients_info.csv')
+        if self.config.demographics == 'all':
+            columns = patients_info.columns
+        else:
+            columns = self.config.demographics
+
+        background = {
+            'PID': patients_info['PID'].tolist() * len(columns),
+        }
+        background['CONCEPT'] = sum([patients_info[col] for col in columns], [])
+        if self.config.ages: background['AGE'] = 0
+        if self.config.abspos: background['ABSPOS'] = 0
+        if self.config.segments: background['SEGMENT'] = 0
+
+        background = pd.DataFrame(background)
+
+        return pd.concat([background, concepts])
+        
+
---
+++ data/loadclass.py
@@ -0,0 +1,48 @@
+from creators import ConceptCreator, AgeCreator, AbsposCreator, SegmentCreator, DemographicsCreator
+
+
+class FeatureMaker():
+    def __init__(self, config, tokenizer):
+        self.config = config
+        self.tokenizer = tokenizer
+
+        self.features = {}
+
+        self.pipeline = self.create_pipeline()
+
+    def __call__(self):
+        concepts = None
+        for creator in self.pipeline:
+            concepts = creator(concepts)
+
+        features = self.create_features(concepts)
+
+        return features
+    
+    def create_pipeline(self):
+        pipeline = [ConceptCreator(self.config)]
+        self.features['CONCEPT'] = []
+
+        if 'ages' in self.config:
+            pipeline.append(AgeCreator(self.config))
+            self.features['AGE'] = []
+        if 'abspos' in self.config:
+            pipeline.append(AbsposCreator(self.config))
+            self.features['ABSPOS'] = []
+        if 'segments' in self.config:
+            pipeline.append(SegmentCreator(self.config))
+            self.features['SEGMENT'] = []
+        if 'demographics' in self.config:
+            pipeline.append(DemographicsCreator(self.config))
+
+
+        return pipeline
+
+    def create_features(self, concepts):
+        def add_to_features(patient):
+            for feature in self.features.keys():
+                self.features[feature].append(patient[feature].tolist())
+        concepts.groupby('PID').apply(add_to_features)
+
+        return self.features
+

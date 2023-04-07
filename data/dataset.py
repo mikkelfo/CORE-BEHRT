f52da5819858c5e0fea52916e2925df461668663
Kiril Klein <kikl@di.ku.dk>
2023-04-07 11:18:57 +0200

    working on hierarchical MLM dataset moved load_vocabulary to BaseDataset

--- data/dataset.py
+++ data/dataset.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset
 import torch
-
+from typing import Dict, List
+import numpy as np
 
 class BaseDataset(Dataset):
     def __init__(self, features: dict, **kwargs):
@@ -19,6 +20,13 @@ class BaseDataset(Dataset):
             raise ValueError('No segment data found. Please add segment data to dataset')
         return max([max(segment) for segment in self.features['segment']]) + 1
 
+    def load_vocabulary(self, vocabulary):
+        if isinstance(vocabulary, str):
+            return torch.load(vocabulary)
+        elif isinstance(vocabulary, dict):
+            return vocabulary
+        else:
+            raise TypeError(f'Unsupported vocabulary input {type(vocabulary)}')
 
 class MLMDataset(BaseDataset):
     def __init__(self, features: dict, **kwargs):
@@ -72,11 +80,84 @@ class MLMDataset(BaseDataset):
 
         return masked_concepts, target
 
-    def load_vocabulary(self, vocabulary):
-        if isinstance(vocabulary, str):
-            return torch.load(vocabulary)
-        elif isinstance(vocabulary, dict):
-            return vocabulary
+    
+
+class H_MLM_Dataset(BaseDataset):
+    def __init__(self, features, **kwargs):
+        super().__init__(features, **kwargs)
+        self.h_vocabulary = self.load_vocabulary(self.kwargs.get('h_vocabulary', 'h_vocabulary.pt'))
+        
+    def __getitem__(self, index):
+        """
+        return: dictionary with codes for patient with index 
+        """ 
+        patient = super().__getitem__(index)
+        mask = self.get_mask(patient)
+        patient['attention_mask'] = mask
+        ids, labels = self.random_mask_ids(patient['idx']) 
+        patient['values'] = self.mask_values(ids, patient['values']) # otherwise model could infer e.g. lab test
+        # pad code sequence, segments and label
+        #patient['codes'] = codes
+        patient['idx'] = ids
+        patient['labels'] = labels
+        for channel in self.channels+['labels', 'idx']:
+            out_dic[channel] = self.seq_padding(patient[channel], 
+                self.pad_tokens[channel])    
+            #print(channel, out_dic[channel])    
+            out_dic[channel] = torch.LongTensor(out_dic[channel])    
+            
+        return out_dic
+
+    def __len__(self):
+        return len(self.data)
+    
+    def mask_values(self, ids, values):
+        """Mask values the same way ids were masked"""
+        mask_id = self.vocab['[MASK]']
+        mask = np.array(ids)==mask_id
+        values = np.array(values)
+        values[mask] = 1
+        return values.tolist()
+
+    def get_mask(self, patient):
+        mask = np.ones(self.pad_len)
+        mask[len(patient['codes']):] = 0
+        return mask
+
+    def init_pad_len(self, data, pad_len):
+        if isinstance(pad_len, type(None)):
+            lens = np.array([len(d['codes']) for d in data])
+            self.pad_len = int(np.max(lens)) 
         else:
-            raise TypeError(f'Unsupported vocabulary input {type(vocabulary)}')
+            self.pad_len = pad_len
+
+    def init_nonspecial_ids(self):
+        """We use by default < as special sign for special tokens"""
+        self.nonspecial_ids = [v for k,v in self.vocab.items() if not k.startswith('[')]
+        
+    def seq_padding(self, seq, pad_token):
+        """Pad a sequence to the given length."""
+        return seq + (self.pad_len-len(seq)) * [pad_token]
+
+    def random_mask_ids(self, ids, seed=0):
+        """mask code with 15% probability, 80% of the time replace with [MASK], 
+            10% of the time replace with random token, 10% of the time keep original"""
+        rng = default_rng(seed)
+        masked_ids = ids.copy()
+        labels = len(ids) * [-100] 
+        for i, id in enumerate(ids):
+            if id not in self.nonspecial_ids:
+                continue
+            prob = rng.uniform()
+            if prob<self.mask_prob:
+                prob = rng.uniform()  
+                # 80% of the time replace with [MASK] 
+                if prob < 0.8:
+                    masked_ids[i] = self.vocab['[MASK]']
+                # 10% change token to random token
+                elif prob < 0.9:      
+                    masked_ids[i] = rng.choice(self.nonspecial_ids) 
+                # 10% keep original
+                labels[i] = id
+        return masked_ids, labels
 

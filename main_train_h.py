f8cd372cc2d9ce618724a5de61e6332f863699da
Kiril Klein <kikl@di.ku.dk>
2023-04-10 12:58:46 +0200

    Add script for hierarchical training
    We can think about dropping it, since the only thing that changes is the model

---
+++ main_train_h.py
@@ -0,0 +1,53 @@
+import torch
+from hydra import initialize, compose
+from hydra.utils import instantiate
+from torch.optim import AdamW
+
+from trainer.trainer import EHRTrainer
+from model.model import HierarchicalBertEHRModel
+from transformers import BertConfig
+
+
+
+def main():
+    with initialize(config_path='configs'):
+        cfg: dict = compose(config_name='trainer.yaml')
+
+    train_dataset = torch.load(cfg.get('train_dataset', 'dataset.train'))
+    val_dataset = torch.load(cfg.get('val_dataset', 'dataset.val'))
+
+    model = HierarchicalBertEHRModel(
+        BertConfig(
+            vocab_size=len(train_dataset.vocabulary),
+            type_vocab_size=train_dataset.max_segments,
+            **cfg.get('model', {}),
+        )
+    )
+
+    opt = cfg.get('optimizer', {})
+    optimizer = AdamW(
+        model.parameters(),
+        lr=opt.get('lr', 1e-5),
+        weight_decay=opt.get('weight_decay', 0.01),
+        eps=opt.get('epsilon', 1e-8),
+    )
+    
+    # Instantiate metrics
+    if 'metrics' in cfg:
+        metrics = {k: instantiate(v) for k, v in cfg.metrics.items()}
+    else:
+        metrics = None
+
+    trainer = EHRTrainer( 
+        model=model, 
+        optimizer=optimizer,
+        train_dataset=train_dataset, 
+        val_dataset=val_dataset, 
+        args=cfg.get('trainer_args', {}),
+        metrics=metrics,
+    )
+    trainer.train()
+
+
+if __name__ == '__main__':
+    main()

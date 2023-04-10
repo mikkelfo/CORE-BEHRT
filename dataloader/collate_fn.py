867d544f6ced9da3e6202207de55c847e3bf59db
Kiril Klein <kikl@di.ku.dk>
2023-04-10 14:32:29 +0200

    Fixed padding and added option for hierarchical target
    changed padding of targets from 0 to ignore_index (-100)

--- dataloader/collate_fn.py
+++ dataloader/collate_fn.py
@@ -9,7 +9,7 @@ def static(data: list):
     return padded_data
 
 
-def dynamic_padding(data: list):
+def dynamic_padding(data: list, hierarchical: bool = False):
     max_len = max([len(patient['concept']) for patient in data])
     for patient in data:
         difference = max_len - len(patient['concept'])
@@ -18,7 +18,15 @@ def dynamic_padding(data: list):
                 dtype = torch.float32
             else:
                 dtype = torch.long
-            patient[key] = torch.cat((torch.tensor(values, dtype=dtype), torch.zeros(difference, dtype=dtype)), dim=0)
+            
+            if key != 'target':
+                patient[key] = torch.cat((torch.tensor(values, dtype=dtype), torch.zeros(difference, dtype=dtype)), dim=0)
+            else:
+                targets = torch.tensor(values, dtype=dtype)
+                if hierarchical:
+                    patient[key] = torch.cat((targets, -100*torch.ones(size=(difference, targets.shape[1]), dtype=dtype)), dim=0)
+                else:
+                    patient[key] = torch.cat((targets, -100*torch.ones(size=(difference,), dtype=dtype)), dim=0)
     
     padded_data = {
         key: torch.stack([patient[key] for patient in data])

eaddca2be6ddd653362384cfb0e46933cae38f4c
Mikkelfo <mikk528a@gmail.com>
2023-05-10 19:49:42 +0200

    Implemented exclusions of rare concepts (non-default function)

--- data_fixes/exclude.py
+++ data_fixes/exclude.py
@@ -2,10 +2,27 @@ import pandas as pd
 
 class Excluder():
     def __call__(self, features: dict, outcomes: dict, k: int = 2) -> pd.DataFrame:
-        return self.exclude_by_k(features, outcomes, k=k)
+        # Exclude patients with few concepts
+        features, outcomes = self.exclude_short_sequences(features, outcomes, k=k)
+        return features, outcomes
+
+    @staticmethod   # Currently unused
+    def exclude_rare_concepts(features: dict, k: int=2) -> pd.DataFrame:
+        unique_codes = {}
+        for patient in features['concept']:
+            for code in patient:
+                unique_codes[code] = unique_codes.get(code, 0) + 1
+        exclude = {code for code, count in unique_codes.items() if count < k}
+
+        for i, patient in enumerate(features['concept']):
+            kept_indices = [idx for idx, code in enumerate(patient) if not code in exclude]
+            for key, values in features.items():
+                features[key][i] = [values[i][j] for j in kept_indices]
+
+        return features
     
     @staticmethod
-    def exclude_by_k(features: dict, outcomes: dict, k: int = 2) -> pd.DataFrame:
+    def exclude_short_sequences(features: dict, outcomes: dict, k: int = 2) -> tuple:
         kept_indices = []
         for i, concepts in enumerate(features['concept']):
             unique_codes = set([code for code in concepts if not code.startswith('[')])
@@ -19,3 +36,4 @@ class Excluder():
             outcomes[key] = [values[i] for i in kept_indices]
 
         return features, outcomes
+

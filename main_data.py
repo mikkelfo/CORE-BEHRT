88fb2610fabce0f303b606ff9f932693832311b0
Kiril Klein <kikl@di.ku.dk>
2023-05-29 12:33:50 +0200

    Process data in batches

--- main_data.py
+++ main_data.py
@@ -2,6 +2,7 @@ import torch
 import hydra
 from omegaconf import OmegaConf
 import json
+from data.dataset import MLMLargeDataset
 from data.concept_loader import ConceptLoader
 from data_fixes.infer import Inferrer
 from data.featuremaker import FeatureMaker
@@ -10,6 +11,9 @@ from data_fixes.exclude import Excluder
 from data.tokenizer import EHRTokenizer
 from data.split import Splitter
 from downstream_tasks.outcomes import OutcomeMaker
+import numpy as np
+from os.path import join
+import os
 
 @hydra.main(config_path="configs/data", config_name="data")
 def main_data(cfg):
@@ -29,47 +33,61 @@ def main_data(cfg):
         Tokenizes
         Saves
     """
-    # Load concepts
-    concepts, patients_info = ConceptLoader()(**cfg.loader)
 
-    # Infer missing values
-    concepts = Inferrer()(concepts)
+    def split_batches(num_batches, split_ratios):
+        batches = np.arange(num_batches)
+        np.random.shuffle(batches)
+        # calculate the number of batches for each set
+        train_end = int(split_ratios['train'] * len(batches))
+        val_end = train_end + int(split_ratios['val'] * len(batches))
+        # split the batches into train, validation and test
+        train_batches = batches[:train_end]
+        val_batches = batches[train_end:val_end]
+        test_batches = batches[val_end:]
+        return train_batches, val_batches, test_batches
 
-    # Make outcomes
-    patients_info = OutcomeMaker(cfg)(patients_info)
+    def tokenize(tokenzier, batches, mode='train'):
+        for batch in batches:
+            features = torch.load(join(cfg.out_dir, f'features{batch}.pt'))
+            train_encoded = tokenizer(features)
+            torch.save(train_encoded, join(cfg.out_dir, f'encoded_{mode}{batch}.pt'))
 
-    # Create feature sequences and outcomes
-    features, outcomes = FeatureMaker(cfg)(concepts, patients_info)
+
+    num_batches = 0
+    for i, concept_batch, patient_batch in enumerate(ConceptLoader(**cfg.loader)):
+        print("Loading concepts...")
+        concepts, patients_info = ConceptLoader()(**cfg.loader)
+        concepts = Inferrer()(concepts)
+        patients_info = OutcomeMaker(cfg)(patients_info)
+        print("Creating feature sequences")
+        features = FeatureMaker(cfg)(concepts, patients_info)
+        features = Handler()(features)
+
+        print("Exclude patients with <k concepts")
+        features = Excluder()(features, k=cfg.min_concepts)
+        torch.save(features, join(cfg.out_dir, f'features{i}.pt'))
+        num_batches += 1
+    # split batches into train, test, val
+    train_batches, val_batches, test_batches = split_batches(num_batches, cfg.split_ratios)
     
-    # Overwrite nans and other incorrect values
-    features = Handler()(features)
-
-    # Exclude patients with <k concepts
-    features, outcomes = Excluder()(features, outcomes)
-
-    # Save final features and outcomes
-    torch.save(features, 'features.pt')
-    torch.save(outcomes, 'outcomes.pt')
-
-    # Split
-    train_features, test_features, val_features = Splitter()(features)
-    train_outcomes, test_outcomes, val_outcomes = Splitter()(outcomes)
-
     # Tokenize
-    tokenizer = EHRTokenizer(cfg.tokenizer)
-    train_encoded = tokenizer(train_features)
+    # Loop through train batches before freezing tokenizer
+    if not os.path.exists(cfg.out_dir):
+        os.makedirs(cfg.out_dir)
+    tokenizer = EHRTokenizer(config=cfg.tokenizer)        
+    train_files = tokenize(tokenizer, train_batches, mode='train')
     tokenizer.freeze_vocabulary()
-    test_encoded = tokenizer(test_features)
-    val_encoded = tokenizer(val_features)
-
-    # Save features and outcomes
-    torch.save(train_encoded, 'train_encoded.pt')
-    torch.save(train_outcomes, 'train_outcomes.pt')
-    torch.save(test_encoded, 'test_encoded.pt')
-    torch.save(test_outcomes, 'test_outcomes.pt')
-    torch.save(val_encoded, 'val_encoded.pt')
-    torch.save(val_outcomes, 'val_outcomes.pt')
+    tokenizer.save_vocab(join(cfg.out_dir, 'vocabulary.pt'))
+    val_files = tokenize(tokenizer, val_batches, mode='val')
+    test_files = tokenize(tokenizer, test_batches, mode='test')
     
+    print("Dataset with MLM and prolonged length of stay")
+    train_dataset = MLMLargeDataset(train_files, vocabulary=tokenizer.vocabulary, **cfg.dataset)
+    test_dataset = MLMLargeDataset(test_files, vocabulary=tokenizer.vocabulary, **cfg.dataset)
+    val_dataset = MLMLargeDataset(val_files, vocabulary=tokenizer.vocabulary, **cfg.dataset)
+    torch.save(train_dataset, 'train_dataset.pt')
+    torch.save(test_dataset, 'test_dataset.pt')
+    torch.save(val_dataset, 'val_dataset.pt')
 
 if __name__ == '__main__':
     main_data()

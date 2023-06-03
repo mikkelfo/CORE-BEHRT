899f13779338e748caadf632407105af355160ba
Kiril Klein <kikl@di.ku.dk>
2023-06-03 19:19:39 +0200

    Change PrecisionAtk to class

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -1,24 +1,27 @@
 import torch
 
 """Computes the precision@k for the specified value of k"""
-def top_k(outputs, batch, topk=10, average=True) -> dict:
-    logits = outputs.logits
-    target = batch['target']
-    
-    ind = torch.where((target != -100) & (target != 0))
+class PrecisionAtK:
+    def __init__(self, topk=10):
+        """Computes the precision@k for the specified value of k"""
+        self.topk = topk
 
-    logits = logits[ind]
-    target = target[ind]
+    def __call__(self, outputs, batch):
+        logits = outputs.prediction_logits
+        target = batch['target']
+        
+        ind = torch.where((target != -100) & (target != 0))
 
-    _, pred = logits.topk(topk, 1, True, True)
-    pred = pred.t()
-    correct = pred.eq(target)
-    if correct.numel() == 0:
-        return 0
-    if not average:
-        return correct.any(0).float()
-    else:
-        return correct.any(0).float().mean().item()
+        logits = logits[ind]
+        target = target[ind]
+
+        _, pred = logits.topk(self.topk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target)
+        if correct.numel() == 0:
+            return 0
+        else:
+            return correct.any(0).float().mean().item()
 
 def binary_hit(outputs, batch, threshold=0.5, average=True):
     logits = outputs.logits

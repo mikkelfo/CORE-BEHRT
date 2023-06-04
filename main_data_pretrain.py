fa8c4eab60e9b627e44fe87eb239507d4d17e0a3
Kiril Klein <kikl@di.ku.dk>
2023-06-04 14:56:11 +0200

    Fix bugs

--- main_data_pretrain.py
+++ main_data_pretrain.py
@@ -5,7 +5,6 @@ from shutil import copyfile
 import torch
 from tqdm import tqdm
 
-from data import utils
 # from data.batch import 
 from data.config import load_config, Config
 from data.concept_loader import ConceptLoader
@@ -19,19 +18,7 @@ from data_fixes.infer import Inferrer
 import numpy as np  
 
 config_path = join("configs", "data.yaml")
-cfg = utils.load_config(config_path)
-"""
-        Loads data
-        Infers nans
-        Finds outcomes
-        Creates features
-        Handles wrong data
-        Excludes patients with <k concepts
-        Splits data
-        Tokenizes
-        Saves
-    """
-
+cfg = load_config(config_path)
 
 def prepare_directory(cfg: Config):
     """Creates output directory and copies config file"""
@@ -39,83 +26,88 @@ def prepare_directory(cfg: Config):
         os.makedirs(cfg.output_dir)
     copyfile(config_path, join(cfg.output_dir, 'data_config.yaml'))
 
-
-
-class BatchTokenize:
-    def __init__(self, tokenizer, cfg: Config):
-        self.tokenizer = tokenizer
-        self.cfg = cfg
-
-    def tokenize(self, batches)-> tuple[list[str]]:
-        print('Tokenizing')
-        train_batches, val_batches, test_batches = batches.train.batches, batches.val.batches, batches.test.batches
-        train_files = self.batch_tokenize(cfg, self.tokenizer, train_batches, mode='train')
-        self.tokenizer.freeze_vocabulary()
-        self.tokenizer.save_vocab(join(cfg.output_dir, 'vocabulary.pt'))
-        val_files = self.batch_tokenize(cfg, self.tokenizer, val_batches, mode='val')
-        test_files = self.batch_tokenize(cfg, self.tokenizer, test_batches, mode='test')
-        return train_files, val_files, test_files
-
-    def batch_tokenize(self, batches, mode='train'):
-        """Tokenizes batches and saves them"""
-        files = []
-        for batch in tqdm(batches, desc=f'Tokenizing {mode} batches'):
-            features = torch.load(join(cfg.output_dir, f'features{batch}.pt'))
-            train_encoded = self.tokenizer(features)
-            torch.save(train_encoded, join(cfg.output_dir, f'encoded_{mode}{batch}.pt'))
-            files.append(join(self.cfg.output_dir, f'encoded_{mode}{batch}.pt'))
-        return files
+    
+class DataSet:
+    def __init__(self):
+        self.pids = None
+        self.file_ids = None
 
 class Batches:
     def __init__(self, cfg: Config, pids: list[list[str]]):
         self.pids = pids
         self.cfg = cfg
         self.split_ratios = cfg.split_ratios
+        self.train = DataSet()
+        self.val = DataSet()
+        self.test = DataSet()
 
-    def split_and_save(self)-> dict:
+    def split_and_save(self)-> None:
         """
         Splits batches into train, val, test and saves them
-        Returns: Dictionary with train, val, test batches and pids
         """
-        train_batches, val_batches, test_batches = self.split_batches(self.pids, cfg.split_ratios)
-        train_pids, val_pids, test_pids = self.split_pids(train_batches, val_batches, test_batches)
+        self.split_batches()
+        self.split_pids()
         
-        self.train.pids = self.flatten(train_pids)
-        self.val.pids = self.flatten(val_pids)
-        self.test.pids = self.flatten(test_pids)
-        self.train.batches = train_batches
-        self.val.batches = val_batches
-        self.test.batches = test_batches
-
-        torch.save(train_pids, join(cfg.output_dir, 'train_pids.pt'))
-        torch.save(val_pids, join(cfg.output_dir, 'val_pids.pt'))
-        torch.save(test_pids, join(cfg.output_dir, 'test_pids.pt'))
+        torch.save(self.train.pids, join(cfg.output_dir, 'train_pids.pt'))
+        torch.save(self.val.pids, join(cfg.output_dir, 'val_pids.pt'))
+        torch.save(self.test.pids, join(cfg.output_dir, 'test_pids.pt'))
     
-    def split_batches(self):
+    def split_batches(self)-> None:
         """Splits the batches into train, validation and test sets"""
-        batches = np.arange(len(self.pids))
-        np.random.shuffle(batches)
+        file_ids = np.arange(len(self.pids))
+        np.random.shuffle(file_ids)
         # calculate the number of batches for each set
-        train_end = int(self.split_ratios['train'] * len(batches))
-        val_end = train_end + int(self.split_ratios['val'] * len(batches))
+        train_end = int(self.split_ratios['train'] * len(file_ids))
+        val_end = train_end + int(self.split_ratios['val'] * len(file_ids))
         # split the batches into train, validation and test
         
-        train_batches = batches[:train_end]
-        val_batches = batches[train_end:val_end]
-        test_batches = batches[val_end:]
+        self.train.file_ids = file_ids[:train_end]
+        self.val.file_ids = file_ids[train_end:val_end]
+        self.test.file_ids = file_ids[val_end:]
 
-        return train_batches, val_batches, test_batches
-
-    def split_pids(self, train_batches, val_batches, test_batches):
+    def split_pids(self)-> None:
         """Splits the pids into train, validation and test sets"""
-        return self.get_pids(train_batches), self.get_pids(val_batches), self.get_pids(test_batches)
+        self.train.pids, self.val.pids, self.test.pids = self.get_pids('train'), self.get_pids('val'), self.get_pids('test')
     
-    def get_pids(self, indices: list):
+    def get_pids(self, set_: str)-> list[str]:
         """Returns the pids for the given indices"""
-        return [self.pids[i] for i in indices]
+        if set_ == 'train':
+            file_ids = self.train.file_ids
+        elif set_ == 'val':
+            file_ids = self.val.file_ids    
+        elif set_ == 'test':
+            file_ids = self.test.file_ids
+        else:
+            raise ValueError(f'Invalid set {set_}')
+        return self.flatten([self.pids[i] for i in file_ids])
+        
     @staticmethod
-    def flatten(ls_of_ls: list[list]):
+    def flatten(ls_of_ls: list[list])-> list:
         return [item for sublist in ls_of_ls for item in sublist] 
+
+class BatchTokenize:
+    def __init__(self, tokenizer, cfg: Config):
+        self.tokenizer = tokenizer
+        self.cfg = cfg
+
+    def tokenize(self, batches: Batches)-> tuple[list[str]]:
+        print('Tokenizing')
+        train_files = self.batch_tokenize(batches.train.file_ids, mode='train')
+        self.tokenizer.freeze_vocabulary()
+        self.tokenizer.save_vocab(join(cfg.output_dir, 'vocabulary.pt'))
+        val_files = self.batch_tokenize(batches.val.file_ids, mode='val')
+        test_files = self.batch_tokenize(batches.test.file_ids, mode='test')
+        return train_files, val_files, test_files
+
+    def batch_tokenize(self, batches, mode='train'):
+        """Tokenizes batches and saves them"""
+        files = []
+        for batch in tqdm(batches, desc=f'Tokenizing {mode} batches'):
+            features = torch.load(join(cfg.output_dir, f'features_{batch}.pt'))
+            train_encoded = self.tokenizer(features)
+            torch.save(train_encoded, join(cfg.output_dir, f'encoded_{mode}_{batch}.pt'))
+            files.append(join(self.cfg.output_dir, f'encoded_{mode}_{batch}.pt'))
+        return files
 
 def main_data(cfg):
     """
@@ -147,7 +139,7 @@ def main_data(cfg):
         # print(features_batch['concept'][0])
         # features_batch = handler(features_batch)
         # features_batch, _ = excluder(features_batch)
-        torch.save(features_batch, join(cfg.output_dir, f'features{i}.pt'))
+        torch.save(features_batch, join(cfg.output_dir, f'features_{i}.pt'))
     batches = Batches(cfg, pids)
     batches.split_and_save()
 

5e3492cfd29527f6cd9cc08360a62f7349f481f0
Kiril Klein <kikl@di.ku.dk>
2023-06-25 17:05:11 +0200

    Hierarchical Large Dataset

--- common/loader.py
+++ common/loader.py
@@ -1,9 +1,16 @@
 from data.dataset import MLMLargeDataset
-
+from data.dataset import HierarchicalLargeDataset
 
 def create_datasets(cfg):
     data_path = cfg.paths.data_path
     train_dataset = MLMLargeDataset(data_path, 'train', **cfg.dataset)
     val_dataset = MLMLargeDataset(data_path, 'val', **cfg.dataset)
+    vocabulary = train_dataset.vocabulary
+    return train_dataset, val_dataset, vocabulary
+
+def create_hierarchical_dataset(cfg):
+    data_path = cfg.paths.data_path
+    train_dataset = HierarchicalLargeDataset(data_path, 'train', **cfg.dataset)
+    val_dataset = HierarchicalLargeDataset(data_path, 'val', **cfg.dataset)
     vocabulary = train_dataset.vocabulary
     return train_dataset, val_dataset, vocabulary

53f7588e597c0a37a36fb1acdd0c638051594fba
Kiril Klein <kikl@di.ku.dk>
2023-06-25 17:04:52 +0200

    Adjust for azure

--- main_h_pretrain.py
+++ main_h_pretrain.py
@@ -1,20 +1,41 @@
-import torch
-import hydra
-from torch.optim import AdamW
-from data.dataset import HierarchicalDataset
+from os.path import join
 
-from trainer.trainer import EHRTrainer
+from common.config import load_config
+from common.loader import create_hierarchical_dataset
+from common.setup import setup_run_folder
 from model.model import HierarchicalBertForPretraining
+from torch.optim import AdamW
+from trainer.trainer import EHRTrainer
 from transformers import BertConfig
 
-@hydra.main(config_path="configs/train", config_name="pretrain_h")
-def main_train(cfg):
+# from azure_run.run import Run
+# from azure_run import datastore
+# from azureml.core import Dataset
+
+
+# run = Run
+run = None
+# run.name(f"Pretrain hierarchical diagnosis medication")
+
+# ds = datastore("workspaceblobstore")
+# dataset = Dataset.File.from_files(path=(ds, 'PHAIR'))
+
+def main_train(config_path):
+    cfg = load_config(config_path)
+    
+    # mount_context = dataset.mount()
+    # mount_context.start()  # this will mount the file streams
+    
+    # cfg.paths.data_path = join(mount_context.mount_point, cfg.paths.data_path)
+    cfg.paths.output_path = join('outputs', cfg.paths.output_path)
+    
+
+    logger = setup_run_folder(cfg)
+    logger.info(f"Access data from {cfg.paths.data_path}")
+    logger.info('Loading data')
+
     # MLM specific
-    train_encoded = torch.load(cfg.paths.train_encoded)
-    val_encoded = torch.load(cfg.paths.val_encoded)
-    vocabulary = torch.load(cfg.paths.vocabulary)
-    train_dataset = HierarchicalDataset(train_encoded, vocabulary=vocabulary, ignore_special_tokens=cfg.ignore_special_tokens)
-    val_dataset = HierarchicalDataset(val_encoded, vocabulary=vocabulary, ignore_special_tokens=cfg.ignore_special_tokens)
+    train_dataset, val_dataset, vocabulary = create_hierarchical_dataset(cfg)
 
     model = HierarchicalBertForPretraining(
         BertConfig(

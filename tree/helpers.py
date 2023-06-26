955d1f67a78c4d74d79898c10aee36f6adfcb9e0
Kiril Klein <kikl@di.ku.dk>
2023-06-26 09:05:25 +0200

    Introduce treebuilder class

--- tree/helpers.py
+++ tree/helpers.py
@@ -8,6 +8,98 @@ from tqdm import tqdm
 from tree.node import Node
 
 
+class TreeBuilder:
+    """
+    Build the tree structure from given file.
+    Returns:
+    A tree structure.
+    """
+    def __init__(self, file='data_dumps/sks_dump_columns.csv', counts=None, cutoff_level=5):
+        self.file = file
+        self.counts = counts
+        self.cutoff_level = cutoff_level
+        self.codes = self.create_levels()
+
+    def build(self):
+        tree = self.create_tree()
+        tree.add_background()
+        tree.cutoff_at_level(self.cutoff_level)
+        tree.extend_leaves(self.cutoff_level)
+        if self.counts is None:
+            self.counts = torch.load('base_counts.pt')
+        tree.base_counts(self.counts)
+        tree.sum_counts()
+        tree.redist_counts()
+        return tree
+
+    def create_levels(self):
+        """
+        Create levels of tree structure.
+        Returns:
+        A list of tuples with levels and codes.
+        """
+        with open(self.file, 'r') as f:
+            df = pd.read_csv(f)
+        codes = []
+        level = -1
+        prev_code = ''
+        for i, (code, text) in df.iterrows():
+            level, prev_code = self.update_level_and_code(level, prev_code, code, text)
+            codes.append((level, code))
+        return codes
+
+    def update_level_and_code(self, level, prev_code, code, text):
+        """
+        Update level and code based on the given code and text.
+        Returns:
+        An updated level and code.
+        """
+        if pd.isna(code):
+            code, level = self.handle_nan_code(level, prev_code, text)
+        else:
+            level += len(code) - len(prev_code)
+            prev_code = code 
+        return level, prev_code
+
+    def handle_nan_code(self, level, prev_code, text):
+        """
+        Handle cases where the code is NaN.
+        Returns:
+        The updated code and level.
+        """
+        if text[:3].lower() == 'kap':
+            code = 'XX'
+            level += 2 - len(prev_code)
+        else:
+            code = 'XXX'
+            level += 3 - len(prev_code)
+        prev_code = code 
+        code = text.split()[-1]
+        return code, level
+
+    def create_tree(self):
+        root = Node('root')
+        parent = root
+        for i in range(len(self.codes)):
+            level, code = self.codes[i]
+            next_level = self.codes[i+1][0] if i < len(self.codes)-1 else level
+            dist = next_level - level 
+            parent, dist = self.update_parent_and_dist(parent, dist, code)
+        return root 
+
+    def update_parent_and_dist(self, parent, dist, code):
+        if dist >= 1:
+            for _ in range(dist):
+                parent.add_child(code)
+                parent = parent.children[-1]
+        elif dist <= 0:
+            parent.add_child(code)
+            for _ in range(0, dist, -1):
+                parent = parent.parent
+        return parent, dist
+
+
+
 def get_counts(cfg, logger):
     """
     Get the counts of unique values from data loaded with the ConceptLoader.
@@ -24,62 +116,4 @@ def get_counts(cfg, logger):
         all_data_counter.update(all_data_batch.value_counts().to_dict())
 
     return all_data_counter
-
-def build_tree(file='data_dumps/sks_dump_columns.csv', counts=None, cutoff_level=5):
-    codes = create_levels(file)
-    tree = create_tree(codes)
-    tree.add_background()
-    tree.cutoff_at_level(cutoff_level)
-    tree.extend_leaves(cutoff_level)
-
-    if counts is None:
-        counts = torch.load('base_counts.pt')
-    tree.base_counts(counts)
-    tree.sum_counts()
-    tree.redist_counts()
-    return tree
-
-def create_levels(file='data_dumps/sks_dump_columns.xlsx'):
-    # df = pd.read_excel(file)
-    df = pd.read_csv(file)
-    codes = []
-    level = -1
-    prev_code = ''
-    for i, (code, text) in df.iterrows():
-        if pd.isna(code):
-            # Manually set nan codes for Chapter and Topic (as they have ranges)
-            if text[:3].lower() == 'kap':
-                code = 'XX'             # Sets Chapter as level 2 (XX)
-            else:
-                if pd.isna(df.iloc[i+1].Kode):  # Skip "subsub"-topics (double nans not started by chapter)
-                    continue
-                code = 'XXX'            # Sets Topic as level 3 (XXX)
-
-        level += len(code) - len(prev_code)  # Add distance between current and previous code to level
-        prev_code = code                # Set current code as previous code
-
-        if code.startswith('XX'):       # Gets proper code (chapter/topic range)
-            code = text.split()[-1]
-
-        codes.append((level, code))
-
-    return codes
-
-def create_tree(codes):
-    root = Node('root')
-    parent = root
-    for i in range(len(codes)):
-        level, code = codes[i]
-        next_level = codes[i+1][0] if i < len(codes)-1 else level
-        dist = next_level - level 
-
-        if dist >= 1:
-            for _ in range(dist):
-                parent.add_child(code)
-                parent = parent.children[-1]
-        elif dist <= 0:
-            parent.add_child(code)
-            for _ in range(0, dist, -1):
-                parent = parent.parent
-    return root  
 

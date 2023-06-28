982aa8e63a7cc4e0770fe51592adb80d54364102
Kiril Klein <kikl@di.ku.dk>
2023-06-28 10:02:21 +0200

    Minor fixes

--- setup_hierarchical.py
+++ setup_hierarchical.py
@@ -1,9 +1,10 @@
+import os
 from os.path import join
 
 import torch
 from common.config import load_config
-from common.setup import prepare_directory
-from tree.helpers import TreeBuilder, get_counts
+from common.setup import prepare_directory_hierarchical
+from tree.helpers import build_tree, get_counts
 
 # from azureml.core import Dataset
 # from azure_run.run import Run
@@ -14,29 +15,30 @@ from tree.helpers import TreeBuilder, get_counts
 
 # dataset = Dataset.File.from_files(path=(datastore(), 'PHAIR/formatted_data/diagnosis_medication'))
 
-config_path = join('configs', 'h_data.yaml')
+config_path = join('configs', 'h_setup.yaml')
 
 def setup_hierarchical(config_path=config_path):
     cfg = load_config(config_path)
-    logger = prepare_directory(config_path, cfg)  
+    logger = prepare_directory_hierarchical(config_path, cfg)  
     logger.info('Mount Dataset')
     # mount_context = dataset.mount()
     # mount_context.start()  # this will mount the file streams
     
-    # cfg.loader.data_dir = mount_context.mount_point
+    # cfg.paths.features = mount_context.mount_point
+    
+    data_dir = cfg.paths.features
     
     logger.info('Get Counts')
-    counts = get_counts(cfg)
+    counts = get_counts(cfg, logger=logger)
     logger.info('Build Tree')
-    tree_builder = TreeBuilder(counts=counts)
-    tree = tree_builder.build()
+    tree = build_tree(counts=counts, cutoff_level=cfg.cutoff_level)
     logger.info('Create Vocabulary')
     vocabulary = tree.create_vocabulary()
 
-    torch.save(vocabulary, join(cfg.output_dir, 'vocabulary.pt'))
-    torch.save(counts, join(cfg.output_dir, 'base_counts.pt'))
-    return tree, vocabulary
+    torch.save(vocabulary, join(data_dir,'hierarchical', 'vocabulary.pt'))
+    torch.save(counts, join(data_dir, 'hierarchical', 'base_counts.pt'))
+    torch.save(tree, join(data_dir, 'hierarchical','tree.pt'))
+    # mount_context.stop()
 
 if __name__ == '__main__':
-    print("Preparing hierarchical data...")
     setup_hierarchical(config_path=config_path)

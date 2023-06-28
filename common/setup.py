1e38027c9d15362ab9ea2be7145b9a7877d1371b
Kiril Klein <kikl@di.ku.dk>
2023-06-28 10:01:21 +0200

    Add hierarchical setup and refactor

--- common/setup.py
+++ common/setup.py
@@ -7,19 +7,32 @@ from shutil import copyfile
 from common.config import Config
 
 
+def create_directory(path: str):
+    """Creates a directory if it doesn't exist."""
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def setup_logger(dir: str, log_file: str = 'info.log'):
+    """Sets up the logger."""
+    logging.basicConfig(filename=join(dir, log_file), level=logging.INFO, 
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    return logging.getLogger(__name__)
+
 def prepare_directory(config_path: str, cfg: Config):
     """Creates output directory and copies config file"""
-    if not os.path.exists(cfg.output_dir):
-        os.makedirs(cfg.output_dir)
+    create_directory(cfg.output_dir)
+    create_directory(join(cfg.output_dir, 'features'))
+    create_directory(join(cfg.output_dir, 'tokenized'))
     copyfile(config_path, join(cfg.output_dir, 'data_config.yaml'))
-    if not os.path.exists(join(cfg.output_dir, 'features')):
-        os.makedirs(join(cfg.output_dir, 'features'))
-    if not os.path.exists(join(cfg.output_dir, 'tokenized')):
-        os.makedirs(join(cfg.output_dir, 'tokenized'))
-    logging.basicConfig(filename=join(cfg.output_dir, 'info.log'), level=logging.INFO, 
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    logger = logging.getLogger(__name__)
-    return logger
+    
+    return setup_logger(cfg.output_dir)
+
+def prepare_directory_hierarchical(config_path: str, cfg: Config):
+    """Creates hierarchical directory and copies config file"""
+    hierarchical_dir = join(cfg.paths.features, "hierarchical")
+    create_directory(hierarchical_dir)
+    copyfile(config_path, join(hierarchical_dir, 'h_setup.yaml'))
+    return setup_logger(hierarchical_dir)
 
 def setup_run_folder(cfg):
     """Creates a run folder"""

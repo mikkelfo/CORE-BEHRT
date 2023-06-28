a97ed4821f98dcc09534f069777e66751af53dc0
Kiril Klein <kikl@di.ku.dk>
2023-06-28 13:58:19 +0200

    Save and load tree

--- common/loader.py
+++ common/loader.py
@@ -1,5 +1,7 @@
 from data.dataset import MLMLargeDataset
 from data.dataset import HierarchicalLargeDataset
+from os.path import join
+import torch
 
 def create_datasets(cfg):
     data_path = cfg.paths.data_path
@@ -10,7 +12,8 @@ def create_datasets(cfg):
 
 def create_hierarchical_dataset(cfg):
     data_path = cfg.paths.data_path
-    train_dataset = HierarchicalLargeDataset(data_path, 'train', **cfg.dataset)
-    val_dataset = HierarchicalLargeDataset(data_path, 'val', **cfg.dataset)
-    vocabulary = train_dataset.vocabulary
-    return train_dataset, val_dataset, vocabulary
+    hierarchical_path = join(data_path, 'hierarchical')
+    tree = torch.load(join(hierarchical_path, 'tree.pt'))
+    train_dataset = HierarchicalLargeDataset(data_path, 'train', tree=tree, **cfg.dataset)
+    val_dataset = HierarchicalLargeDataset(data_path, 'val', tree=tree, **cfg.dataset)
+    return train_dataset, val_dataset

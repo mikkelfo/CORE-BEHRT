0ae60ff7b217b1d9174680f69e93eb1ad3477d79
Kiril Klein <kikl@di.ku.dk>
2023-06-29 17:51:09 +0200

    Refactor, pass leaf size

--- main_h_pretrain.py
+++ main_h_pretrain.py
@@ -7,7 +7,7 @@ from model.model import HierarchicalBertForPretraining
 from torch.optim import AdamW
 from trainer.trainer import EHRTrainer
 from transformers import BertConfig
-
+import torch
 # from azure_run.run import Run
 # from azure_run import datastore
 # from azureml.core import Dataset
@@ -19,6 +19,16 @@ run = None
 
 # ds = datastore("workspaceblobstore")
 # dataset = Dataset.File.from_files(path=(ds, 'PHAIR'))
+config_path = 'configs/h_pretrain.yaml'
+
+def load_hierarchical_data(cfg):
+    """Load hierarchical data from disk"""
+    vocab = torch.load(join(cfg.paths.data_path, 'vocabulary.pt'))
+    tree = torch.load(join(cfg.paths.data_path, 'hierarchical', 'tree.pt'))
+    train_dataset, val_dataset = create_hierarchical_dataset(cfg)
+
+    return vocab, tree, train_dataset, val_dataset
+
 
 def main_train(config_path):
     cfg = load_config(config_path)
@@ -29,21 +39,16 @@ def main_train(config_path):
     # cfg.paths.data_path = join(mount_context.mount_point, cfg.paths.data_path)
     cfg.paths.output_path = join('outputs', cfg.paths.output_path)
     
-
     logger = setup_run_folder(cfg)
-    logger.info(f"Access data from {cfg.paths.data_path}")
-    logger.info('Loading data')
+    
+    logger.info(f'Loading data from {cfg.paths.data_path}')
+    vocab, tree, train_dataset, val_dataset = load_hierarchical_data(cfg)
+  
+    logger.info("Setup model")
+    bertconfig = BertConfig(leaf_size=len(train_dataset.leaf_counts), vocab_size=len(vocab), **cfg.model)
+    model = HierarchicalBertForPretraining(bertconfig, tree=tree)
 
-    # MLM specific
-    train_dataset, val_dataset = create_hierarchical_dataset(cfg)
-
-    model = HierarchicalBertForPretraining(
-        BertConfig(
-            vocab_size=train_dataset.leaf_counts,
-            **cfg.model
-        )
-    )
-
+    logger.info("Setup optimizer")
     optimizer = AdamW(
         model.parameters(),
         lr=cfg.optimizer.lr,
@@ -51,6 +56,7 @@ def main_train(config_path):
         eps=cfg.optimizer.epsilon,
     )
 
+    logger.info("Setup trainer")
     trainer = EHRTrainer( 
         model=model, 
         optimizer=optimizer,
@@ -58,10 +64,12 @@ def main_train(config_path):
         val_dataset=val_dataset, 
         args=cfg.trainer_args,
         metrics=cfg.metrics,
-        cfg=cfg
+        cfg=cfg,
+        logger=logger,
     )
+    breakpoint()
     trainer.train()
 
 
 if __name__ == '__main__':
-    main_train()
+    main_train(config_path)

0fc013d74364a551185dda99b6080db02fc83cf5
Kiril Klein <kikl@di.ku.dk>
2023-06-30 11:13:26 +0200

    Handle hierarchical

--- dataloader/collate_fn.py
+++ dataloader/collate_fn.py
@@ -14,24 +14,25 @@ def dynamic_padding(data: list):
     for patient in data:
         difference = max_len - len(patient['concept'])
         for key, values in patient.items():
-            if key in ['age', 'abspos']:
-                dtype = torch.float32
-            else:
-                dtype = torch.long
-
             if key == 'target':
-                if isinstance(values, float):
+                if isinstance(values, float):           # 0D: For finetuning
                     patient[key] = torch.tensor(values)
                     continue
-                filler = torch.ones(difference, dtype=dtype) * -100
+                elif values.ndim == 1:                  # 1D: For normal pretraining
+                    filler = torch.ones(difference, dtype=values.dtype) * -100
+                elif values.ndim == 3:                  # 3D: For hierarchical pretraining 
+                    filler = torch.ones_like(values) * -100
+                    filler = filler[:difference, :, :]
             else:
-                filler = torch.zeros(difference, dtype=dtype)
-            patient[key] = torch.cat((values.to(dtype), filler), dim=0)
-    
-    padded_data = {
-        key: torch.stack([patient[key] for patient in data])
-        for key in data[0].keys()
-    }
+                filler = torch.zeros(difference, dtype=values.dtype)
+            patient[key] = torch.cat((values, filler), dim=0)
+
+    padded_data = {}
+    for key in data[0].keys():
+        if key == 'target' and data[0]['target'].ndim == 3:                     # For hierarchical pretraining only 
+            padded_data[key] = torch.cat([patient[key] for patient in data])    # We give flat labels as we manually mask them in the loss function
+        else:
+            padded_data[key] = torch.stack([patient[key] for patient in data])
 
     return padded_data
 

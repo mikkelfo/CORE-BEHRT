303c724ecc3eb92fbebfa48d69d763979f3aec8c
Kiril Klein <kikl@di.ku.dk>
2023-07-13 18:04:34 +0200

    Separate forward method for hierarchical impl.
    levels is inferred from the data

--- ehr2vec/model/model.py
+++ ehr2vec/model/model.py
@@ -18,7 +18,7 @@ class BertEHRModel(BertModel):
     def forward(
         self,
         input_ids=None,
-        attention_mask=None, # It's used for both attention and labels mask
+        attention_mask=None, 
         token_type_ids=None,
         position_ids=None,
         inputs_embeds=None,
@@ -37,11 +37,11 @@ class BertEHRModel(BertModel):
         outputs.logits = logits
 
         if labels is not None:
-            outputs.loss = self.get_loss(logits, labels, attention_mask)
+            outputs.loss = self.get_loss(logits, labels)
 
         return outputs
 
-    def get_loss(self, logits, labels, attention_mask=None):
+    def get_loss(self, logits, labels):
         return self.loss_fct(logits.view(-1, self.config.vocab_size), labels.view(-1))
 
 
@@ -61,7 +61,7 @@ class BertForFineTuning(BertEHRModel):
 
 
 class HierarchicalBertForPretraining(BertEHRModel):
-    def __init__(self, config, tree, tree_matrix=None):
+    def __init__(self, config, tree=None, tree_matrix=None):
         super().__init__(config)
 
         self.loss_fct = nn.CrossEntropyLoss(reduction='none')
@@ -69,16 +69,42 @@ class HierarchicalBertForPretraining(BertEHRModel):
 
         # TODO: Make this configurable
         self.linear_combination = torch.arange(config.levels, 0, -1) / config.levels
-        
-        if not tree_matrix:
+
+        if not isinstance(tree_matrix, torch.Tensor):
+            assert tree is not None, "Either tree or tree_matrix must be provided"
             tree_matrix = tree.get_tree_matrix()
 
         self.tree_matrix = tree_matrix
         self.tree_matrix_sparse = self.tree_matrix.to_sparse()
         self.tree_mask = self.tree_matrix.any(2)
 
-    def get_loss(self, logits, labels, labels_mask):
-        levels = 5
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        inputs_embeds=None,
+        labels=None,
+    ):
+        outputs = super().forward(
+            input_ids=input_ids,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            inputs_embeds=inputs_embeds,
+        )
+
+        sequence_output = outputs[0]    # Last hidden state
+        logits = self.cls(sequence_output)
+        outputs.logits = logits
+
+        if labels is not None:
+            outputs.loss = self.get_loss(logits, labels, attention_mask)
+
+        return outputs
+
+    def get_loss(self, logits, labels, target_mask):
+        levels = labels.shape[1]
         batch_size, seq_len, leaves = logits.shape
         logits = logits.permute(2, 0, 1).view(leaves, -1)  # (leaves, batch_size*seq_len)
 
@@ -90,8 +116,9 @@ class HierarchicalBertForPretraining(BertEHRModel):
             else:
                 level_predictions = torch.matmul(self.tree_matrix[i, self.tree_mask[i]], logits)    # (leaves, leaves) @ (leaves, batch_size*seq_len) -> (leaves, batch_size*seq_len)
             level_predictions = level_predictions.transpose(1, 0)
-            level_predictions = level_predictions[labels_mask.view(-1)]
+            level_predictions = level_predictions[target_mask.view(-1)]
             level_labels = labels[:, i, self.tree_mask[i]]
+            
             loss = self.loss_fct(level_predictions, level_labels)
             acc_loss += (loss * self.linear_combination[i]).mean()
         

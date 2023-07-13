ab35598c922e9935fcade8ff9545737611a0dd88
Kiril Klein <kikl@di.ku.dk>
2023-07-13 13:23:36 +0200

    Add standard dataset option to hierarchical

--- ehr2vec/common/loader.py
+++ ehr2vec/common/loader.py
@@ -1,5 +1,5 @@
 from data.dataset import MLMLargeDataset, MLMDataset
-from data.dataset import HierarchicalLargeDataset
+from data.dataset import HierarchicalLargeDataset, HierarchicalDataset
 from os.path import join
 import torch
 
@@ -21,6 +21,13 @@ def create_hierarchical_dataset(cfg):
     data_path = cfg.paths.data_path
     hierarchical_path = join(data_path, 'hierarchical')
     tree = torch.load(join(hierarchical_path, 'tree.pt'))
-    train_dataset = HierarchicalLargeDataset(data_path, 'train', tree=tree, **cfg.dataset)
-    val_dataset = HierarchicalLargeDataset(data_path, 'val', tree=tree, **cfg.dataset)
+    if cfg.large_dataset:
+        train_dataset = HierarchicalLargeDataset(data_path, 'train', tree=tree, **cfg.dataset)
+        val_dataset = HierarchicalLargeDataset(data_path, 'val', tree=tree, **cfg.dataset)
+    else:
+        train_encoded = torch.load(join(data_path, 'tokenized','train_encoded.pt'))
+        val_encoded = torch.load(join(data_path, 'tokenized','val_encoded.pt'))
+        vocabulary = torch.load(join(data_path, 'vocabulary.pt'))
+        train_dataset = HierarchicalDataset(train_encoded, vocabulary=vocabulary, **cfg.dataset)
+        val_dataset = HierarchicalDataset(val_encoded, vocabulary=vocabulary, **cfg.dataset)
     return train_dataset, val_dataset

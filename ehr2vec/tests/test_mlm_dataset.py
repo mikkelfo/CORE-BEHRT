3ff22123142c70251dbedceb52734cfed468cba9
Kiril Klein <kikl@di.ku.dk>
2023-07-14 15:58:15 +0200

    Fixed bug in test

--- ehr2vec/tests/test_mlm_dataset.py
+++ ehr2vec/tests/test_mlm_dataset.py
@@ -15,7 +15,7 @@ class TestMLMDataset(unittest.TestCase):
         mock_mode = 'mode'
         mock_vocabulary = {'[MASK]': 0, '[SEP]': 1, 'a': 2, 'b': 3, 'c': 4, 'd': 5, 'e': 6}
         # Call the function and check the result
-        dataset = MLMDataset(mock_data_dir, mock_mode, vocabulary=mock_vocabulary)
+        dataset = MLMDataset(mock_data_dir, mock_mode, vocabulary=mock_vocabulary, masked_ratio=0.6)
         self.assertEqual(len(dataset), 2)
         self.assertEqual(dataset.max_segments, 3)
 
@@ -29,15 +29,15 @@ class TestMLMDataset(unittest.TestCase):
         mock_vocabulary = {'[MASK]': 0, '[SEP]': 1, 'a': 2, 'b': 3, 'c': 4, 'd': 5, 'e': 6}
         mock_data_dir = 'data_dir'
         mock_mode = 'mode'
-
+        print('original concepts', mock_torch_load.return_value['concept'])
         # Call the function and check the result
-        dataset = MLMDataset(mock_data_dir, mock_mode, vocabulary=mock_vocabulary)
+        dataset = MLMDataset(mock_data_dir, mock_mode, vocabulary=mock_vocabulary, masked_ratio=0.6)
         patient = dataset[0]
-        print(patient)
+        print('masked patient',patient)
         # Check that none of the special tokens are masked
-        special_token_indices = (torch.tensor(mock_torch_load.return_value['concept']) < 2).nonzero(as_tuple=True)[0]
-        self.assertTrue(torch.all(patient['concept'][special_token_indices] != 0))
+        special_token_mask = (torch.tensor(mock_torch_load.return_value['concept'][0]) < 2)
+        print("Special token MASK", special_token_mask)
+        self.assertTrue(torch.all(patient['concept'][special_token_mask] != 0))
         # Check that tokens which have a target not equal to -100 are either a mask token, the original token, or a random non-special token
         target_indices = (patient['target'] != -100).nonzero(as_tuple=True)[0]
         self.assertTrue(torch.all((patient['concept'][target_indices] == 0) | (patient['concept'][target_indices] == patient['target'][target_indices]) | ((patient['concept'][target_indices] >= 2) & (patient['concept'][target_indices] < len(mock_vocabulary)))))
-

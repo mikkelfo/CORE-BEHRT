063b1edd2fc6b19311211306256a48197843bb84
Kiril Klein <kikl@di.ku.dk>
2023-07-23 14:12:50 +0200

    Adjust config

--- ehr2vec/main_embed_concepts.py
+++ ehr2vec/main_embed_concepts.py
@@ -4,7 +4,7 @@ This script is used to embed concepts into a vector space.
 
 import json
 import os
-from os.path import join, split
+from os.path import join
 
 import torch
 import typer
@@ -27,35 +27,35 @@ def main(config_path):
         _, mount_context = azure.setup_azure(cfg.run_name)
         mount_dir = mount_context.mount_dir
         cfg.loader.data_dir = join(mount_dir, cfg.loader.data_dir) # specify paths here
+        cfg.output_dir = join(mount_dir, cfg.output_dir)
     
-    model_path = cfg.paths.model
-    data_path = cfg.paths.data
     
     logger = prepare_embedding_directory(config_path, cfg)  
     
-    model_dir = split(model_path)[0]
-    with open(join(model_dir, 'config.json'), 'r') as f:
+    with open(join(cfg.loader.model_dir, 'config.json'), 'r') as f:
         cfg = json.load(f)['model_config']
     logger.info('Initialize Model')
+    
     model = BertEHRModel(BertConfig(**{**cfg, **{'output_hidden_states':True}}))
+    model_path = join(cfg.loader.model_dir, 'checkpoints', f'checkpoint_epoch{cfg.loader.checkpoint_num}_end.pt')
     state_dict = torch.load(model_path)['model_state_dict']
     model.load_state_dict(state_dict, strict=False)
     
-    # TODO: allow big datasets
-    ds = dataset.BaseSmallDataset(torch.load(data_path))
+    ds = dataset.BaseEHRDataset(cfg.loader.data_dir, mode='val', cfg=cfg)
     
     logger.info('Produce embeddings')
-    data = embeddings.produce_concept_embeddings(model, ds, **cfg.embeddings)
+    forwarder = embeddings.Forwarder(model, cfg)
+    data = forwarder.produce_concept_embeddings(model, ds, **cfg.embeddings)
     
-    emb_save_path = join(model_dir, 'embeddings', f'concept_emb_{cfg.batch_size*cfg.stop_iter}pats.hdf5')
+    emb_save_path = join(cfg.loader, 'embeddings', f'concept_emb_{cfg.batch_size*cfg.stop_iter}pats.hdf5')
     logger.info(f"Save embeddings to {emb_save_path}")
     io.save_hdf5(data, emb_save_path)
     if cfg.project:
         logger.info('Project')
         data = visualization.project_embeddings(data, cfg)
         del data['concept_enc']
-        df = pe.store_to_df(data, data_path)
-        save_path = join(model_dir, f'concept_proj_{cfg.batch_size*cfg.stop_iter}pats.csv') 
+        df = forwarder.store_to_df(data, cfg.output_dir)
+        save_path = join(cfg.output_dir, f'concept_proj_{cfg.batch_size*cfg.stop_iter}pats.csv') 
         df.to_csv(save_path, index=False)
         logger.info(f'Embeddings stored to {save_path}')
 

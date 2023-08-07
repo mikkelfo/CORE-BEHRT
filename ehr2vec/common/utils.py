99a8fcef05d52b472033985bbabe526abd8c5549
Kiril Klein <kikl@di.ku.dk>
2023-08-07 17:09:05 +0200

    Add dataset concatenation

--- ehr2vec/common/utils.py
+++ ehr2vec/common/utils.py
@@ -1,4 +1,14 @@
+from torch.utils.data import IterableDataset
+
 def check_patient_counts(concepts, patients_info, logger):
     if concepts.PID.nunique() != patients_info.PID.nunique():
             logger.warning(f"patients info contains {patients_info.PID.nunique()} patients != \
                         {concepts.PID.nunique()} unique patients in concepts")
+
+class ConcatIterableDataset(IterableDataset):
+    def __init__(self, datasets):
+        self.datasets = datasets
+
+    def __iter__(self):
+        for dataset in self.datasets:
+            yield from dataset

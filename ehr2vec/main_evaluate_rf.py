fdcd9468d578a0c7b494c3561c63e5e597407281
Kiril Klein <kikl@di.ku.dk>
2023-08-20 14:56:54 +0200

    Move hparameter opt to before CV, Refactor

--- ehr2vec/main_evaluate_rf.py
+++ ehr2vec/main_evaluate_rf.py
@@ -1,4 +1,5 @@
 import os
+from collections import defaultdict
 from os.path import join, split
 from shutil import copyfile
 
@@ -10,13 +11,111 @@ from common.io import PatientHDF5Reader
 from common.logger import TqdmToLogger
 from common.setup import get_args, setup_logger
 from evaluation.optimize import find_best_params_RF
-from evaluation.utils import get_mean_std, Oversampler, sample
+from evaluation.utils import Oversampler, get_mean_std, sample
+from joblib import Parallel, delayed
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, train_test_split
 from tqdm import tqdm
 
 args = get_args("evaluate_rf.yaml")
 config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), args.config_path)
+
+
+def main(config_path):
+    cfg = load_config(config_path)
+    cfg.output_dir = join(cfg.model_dir, 'results', 'RF', cfg.run_name)
+    cfg, mount_context = azure_mount(cfg)
+    logger = prepare_eval_directory(config_path, cfg)
+    
+    reader = PatientHDF5Reader(join(cfg.model_dir, 'encodings', 'censored_patients', cfg.uncensored, 'encodings.h5'))
+    X_uncencored, _ = reader.read_arrays()
+    pids_uncensored = reader.read_pids()
+    
+    metrics = [get_function(metric) for metric in cfg.metrics.values()]
+    Results_dic = {}
+    for name in tqdm(cfg.tasks, desc='Tasks',  file=TqdmToLogger(logger)):
+        task = cfg.tasks[name]
+        logger.info(f'Processing task {task}')
+        reader = PatientHDF5Reader(join(cfg.model_dir, 'encodings', 'censored_patients', task.folder, 'encodings.h5'))
+        X, y = get_dataset(X_uncencored, pids_uncensored, reader)
+        logger.info(f"Dataset size: {len(X)}")
+        logger.info(f"Perc. positives: {sum(y)/len(y)}")
+        X, y = sample(X, y, task, logger)
+        
+        logger.info("Use {cfg.optimizer.split} of the data to optimize hyperparameters")
+        X, X_opt, y, y_opt = train_test_split(X, y, test_size=cfg.optimizer.split, random_state=42)
+        best_params = find_best_params_RF(X_opt, y_opt, param_grid=cfg.optimizer.param_grid, **cfg.parallel)
+        logger.info(f"Best params: {best_params}")
+        logger.info(f" {cfg.n_folds}-fold Cross-validate")
+        skf = StratifiedKFold(cfg.n_folds, shuffle=True, random_state=42)
+        # Parallelize the cross-validation process
+        parallel = Parallel(n_jobs=cfg.parallel.n_jobs, temp_folder=cfg.parallel.temp_folder)
+        parallel_results = parallel(delayed(process_fold)(fold, X, y, task, metrics, best_params, logger) for fold in skf.split(X, y))
+        # Aggregate results
+        Results_dic[name] = get_results_dic(parallel_results)
+
+    Results_dic = get_mean_std(Results_dic)
+    Results_df = pd.DataFrame(Results_dic).T
+    Results_df.to_csv(join(cfg.output_dir, f'results.csv'), index_label='task')
+
+    if cfg.env=='azure':
+        from azure_run import file_dataset_save
+        file_dataset_save(local_path=split(cfg.output_dir)[0], datastore_name = "workspaceblobstore",
+                    remote_path = join("PHAIR", cfg.model_dir, 'results', 'RF'))
+        mount_context.stop()
+
+def process_fold(fold, X, y, task, metrics, best_params, logger):
+    train_ids, val_ids = fold
+    X_train, X_val, y_train, y_val = X[train_ids], X[val_ids], y[train_ids], y[val_ids]
+    
+    X_train, y_train = oversample_data(X_train, y_train, task, logger)
+    pred, pred_probas = train_and_predict(X_train, y_train, X_val, best_params, logger)
+    results = evaluate_predictions(y_val, pred, pred_probas, metrics, logger)
+    return results
+
+def train_and_predict(X_train, y_train, X_val, best_params, logger):
+    logger.info("Train model")
+    clf = RandomForestClassifier(**best_params, random_state=42)
+    clf.fit(X_train, y_train)
+    
+    logger.info("Predict")
+    pred = clf.predict(X_val)
+    pred_probas = clf.predict_proba(X_val)[:, 1]
+    
+    return pred, pred_probas
+
+def evaluate_predictions(y_val, pred, pred_probas, metrics, logger):
+    results = {}
+    logger.info("Evaluate")
+    for metric in metrics:
+        score = metric(y_val, pred_probas if metric.__name__.endswith('auc') else pred)
+        logger.info(f"{metric.__name__}: {score}")
+        results[metric.__name__] = score
+    return results
+
+def oversample_data(X, y, task, logger):
+    if "oversampling_ratio" in task and task.get("oversampling_ratio", False):
+        logger.info(f"Oversampling with ratio {task.oversampling_ratio}")
+        oversampler = Oversampler(task.oversampling_ratio)
+        X_train, y_train = oversampler.fit_resample(X_train, y_train)
+        logger.info(f"New dataset size: {len(X_train)}")
+        logger.info(f"New Perc. positives: {sum(y_train)/len(y_train)}")
+    return X, y
+
+def get_results_dic(parallel_results):
+    results = defaultdict(list)
+    for fold_result in parallel_results:
+        for key, value in fold_result.items():
+            results[key].append(value)
+    return results
+
+def sample(X,y, task, logger):
+    if 'sample' in task:
+        logger.info(f"Sampling with {task.sample}")
+        X, y = sample(X, y, **task.sample)
+        logger.info(f"New dataset size: {len(X)}")
+        logger.info(f"New Perc. positives: {sum(y)/len(y)}")
+    return X, y
 
 def azure_mount(cfg):
     if cfg.env=='azure':
@@ -49,69 +148,6 @@ def get_dataset(X_unc, pids_unc, reader, shuffle=True):
         X, y = X[indices], y[indices]
     return X, y
 
-def main(config_path):
-    cfg = load_config(config_path)
-    cfg.output_dir = join(cfg.model_dir, 'results', 'RF', cfg.run_name)
-    cfg, mount_context = azure_mount(cfg)
-    logger = prepare_eval_directory(config_path, cfg)
-    
-    reader = PatientHDF5Reader(join(cfg.model_dir, 'encodings', 'censored_patients', cfg.uncensored, 'encodings.h5'))
-    X_uncencored, _ = reader.read_arrays()
-    pids_uncensored = reader.read_pids()
-    
-    metrics = [get_function(metric) for metric in cfg.metrics.values()]
-    Results_dic = {}
-    for name in tqdm(cfg.tasks, desc='Tasks',  file=TqdmToLogger(logger)):
-        task = cfg.tasks[name]
-        logger.info(f'Processing task {task}')
-        reader = PatientHDF5Reader(join(cfg.model_dir, 'encodings', 'censored_patients', task.folder, 'encodings.h5'))
-        X, y = get_dataset(X_uncencored, pids_uncensored, reader)
-        logger.info(f"Dataset size: {len(X)}")
-        logger.info(f"Perc. positives: {sum(y)/len(y)}")
-
-        if 'sample' in task:
-            logger.info(f"Sampling with {task.sample}")
-            X, y = sample(X, y, **task.sample)
-            logger.info(f"New dataset size: {len(X)}")
-            logger.info(f"New Perc. positives: {sum(y)/len(y)}")
-
-        skf = StratifiedKFold(cfg.n_folds, shuffle=True, random_state=42)
-        results = {metric.__name__:[] for metric in metrics}
-        for fold in tqdm(skf.split(X,y), desc='CV',  file=TqdmToLogger(logger)):
-            train_ids, val_ids = fold
-            X_train, X_val, y_train, y_val = X[train_ids], X[val_ids], y[train_ids], y[val_ids]
-
-            if "oversampling_ratio" in task and task.get("oversampling_ratio", False):
-                logger.info(f"Oversampling with ratio {task.oversampling_ratio}")
-                oversampler = Oversampler(task.oversampling_ratio)
-                X_train, y_train = oversampler.fit_resample(X_train, y_train)
-                logger.info(f"New dataset size: {len(X_train)}")
-                logger.info(f"New Perc. positives: {sum(y_train)/len(y_train)}")
-            logger.info("Optimize hyperparameters")
-            best_params = find_best_params_RF(X_train, y_train, **cfg.optimizer)
-            logger.info(f"Best params: {best_params}")
-            logger.info("Train model")
-            clf = RandomForestClassifier(**best_params, random_state=42)
-            clf.fit(X_train, y_train)
-            logger.info("Predict")
-            pred = clf.predict(X_val)
-            pred_probas = clf.predict_proba(X_val)[:, 1]
-            logger.info("Evaluate")
-            for metric in metrics:
-                score = metric(y_val, pred_probas if metric.__name__.endswith('auc') else pred)
-                logger.info(f"{metric.__name__}: {score}")
-                results[metric.__name__].append(score)
-        Results_dic[name] = results
-
-    Results_dic = get_mean_std(Results_dic)
-    Results_df = pd.DataFrame(Results_dic).T
-    Results_df.to_csv(join(cfg.output_dir, f'results.csv'), index_label='task')
-
-    if cfg.env=='azure':
-        from azure_run import file_dataset_save
-        file_dataset_save(local_path=split(cfg.output_dir)[0], datastore_name = "workspaceblobstore",
-                    remote_path = join("PHAIR", cfg.model_dir, 'results', 'RF'))
-        mount_context.stop()
 
 if __name__ == '__main__':
     main(config_path)

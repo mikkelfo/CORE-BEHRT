92176a0842c8221f6f51073c9ebfa8427da8423b
Kiril Klein <kikl@di.ku.dk>
2023-09-06 15:44:01 +0200

    Closes #83

--- ehr2vec/main_finetune.py
+++ ehr2vec/main_finetune.py
@@ -5,8 +5,9 @@ import pandas as pd
 import torch
 from common.azure import setup_azure
 from common.config import load_config
-from common.loader import create_binary_outcome_datasets, load_model
+from common.loader import load_model, retrieve_outcomes
 from common.setup import get_args, setup_run_folder
+from data.dataset import CensorDataset
 from model.model import BertForFineTuning
 from torch.optim import AdamW
 from torch.utils.data import WeightedRandomSampler
@@ -16,26 +17,6 @@ args = get_args('finetune.yaml')
 
 config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), args.config_path)
 
-def get_sampler(cfg, train_dataset, outcomes, logger):
-    if cfg.trainer_args['sampler']:
-        logger.warning('Sampler does not work with IterableDataset. Use positive weight instead')
-        labels = pd.Series(outcomes).notna().astype(int)
-        label_weight = 1 / labels.value_counts()
-        weights = labels.map(label_weight).values
-        sampler = WeightedRandomSampler(
-            weights=weights,
-            num_samples=len(train_dataset),
-            replacement=True
-        )
-        return sampler
-    else:
-        return None
-
-def get_pos_weight(cfg, outcomes):
-    if cfg.trainer_args['pos_weight']:
-        return sum(pd.isna(outcomes)) / sum(pd.notna(outcomes))
-    else:
-        return None
 
 def main_finetune():
     cfg = load_config(config_path)
@@ -52,8 +33,12 @@ def main_finetune():
     logger.info(f'Outcome file: {cfg.paths.outcome}, Outcome name: {cfg.outcome.type}')
     logger.info(f'Censor file: {cfg.paths.censor}, Censor name: {cfg.outcome.censor_type}')
     logger.info(f"Censoring {cfg.outcome.n_hours} hours after censor_outcome")
-    all_outcomes = torch.load(cfg.paths.outcome)
-    train_dataset, val_dataset, outcomes = create_binary_outcome_datasets(all_outcomes, cfg)
+    all_outcomes = torch.load(join(cfg.paths.data_path, cfg.paths.outcome))
+
+    outcomes, censor_outcomes, pids = retrieve_outcomes(all_outcomes, cfg)
+    # TODO: that should be configurable
+    pids = filter_pids(cfg.paths.model_path, pids, logger)
+    train_dataset, val_dataset = get_datasets(cfg, outcomes, censor_outcomes, pids)
 
     logger.info('Initializing model')
     model = load_model(BertForFineTuning, cfg, {'pos_weight':get_pos_weight(cfg, outcomes)})
@@ -82,6 +67,52 @@ def main_finetune():
                     remote_path = join("PHAIR", 'models', split(cfg.paths.model_path)[-1], "finetune_"+cfg.run_name))
         mount_context.stop()
     logger.info('Done')
+
+
+def get_sampler(cfg, train_dataset, outcomes, logger):
+    if cfg.trainer_args['sampler']:
+        logger.warning('Sampler does not work with IterableDataset. Use positive weight instead')
+        labels = pd.Series(outcomes).notna().astype(int)
+        label_weight = 1 / labels.value_counts()
+        weights = labels.map(label_weight).values
+        sampler = WeightedRandomSampler(
+            weights=weights,
+            num_samples=len(train_dataset),
+            replacement=True
+        )
+        return sampler
+    else:
+        return None
+
+def get_pos_weight(cfg, outcomes):
+    if cfg.trainer_args['pos_weight']:
+        return sum(pd.isna(outcomes)) / sum(pd.notna(outcomes))
+    else:
+        return None
+
+def filter_pids(model_path, pids, logger):
+    """Filter pids based on the pids that have already been trained on."""
+    logger.info('Filtering pids')
+    train_pids = torch.load(join(model_path, 'train_pids.pt'))
+    val_pids = torch.load(join(model_path, 'val_pids.pt'))
+    pt_pids = set(train_pids).union(set(val_pids)) 
+    logger.info(f"Number of pids before filtering: {len(pids)}")
+    ft_pids = [pid for pid in pids if pid not in pt_pids]
+    return ft_pids
+
+def get_datasets(cfg, outcomes, censor_outcomes, pids):
+    kwargs = {
+        'data_dir': cfg.paths.data_path,
+        'outcomes': outcomes,
+        'censor_outcomes': censor_outcomes,
+        'outcome_pids': pids,
+        'pids': pids,
+        'n_hours': cfg.outcome.n_hours,
+        'n_procs': cfg.train_data.get('n_procs', None)
+    }
+    train_dataset = CensorDataset(mode='train', num_patients=cfg.train_data.num_patients, **kwargs)
+    val_dataset = CensorDataset(mode='val', num_patients=cfg.val_data.num_patients, **kwargs)
+    return train_dataset, val_dataset
 
 if __name__ == '__main__':
     main_finetune()

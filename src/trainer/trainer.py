59bcc69c2cb4ead87097e40c2bcde51d0f4c37d7
Mikkelfo <mikk528a@gmail.com>
2023-09-12 13:28:25 +0200

    Removed MPS support due to bugs

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -23,13 +23,9 @@ class EHRTrainer:
         sampler=None,
         cfg=None,
     ):
-        if torch.cuda.is_available():
-            self.device = torch.device("cuda")
-        elif torch.backends.mps.is_available():
-            self.device = torch.device("mps")
-        else:
-            self.device = torch.device("cpu")
-
+        self.device = (
+            torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        )
         print("Running on:", self.device)
 
         self.model = model.to(self.device)

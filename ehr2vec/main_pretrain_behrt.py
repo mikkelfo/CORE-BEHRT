1f4f04ce4dd4f4a45d64a6416ddafedabdfea668
Kiril Klein <kikl@di.ku.dk>
2023-09-12 16:28:39 +0200

    Close #90

---
+++ ehr2vec/main_pretrain_behrt.py
@@ -0,0 +1,120 @@
+"""Pretrain BEHRT model on EHR data. We convert our data to behrt format and run the behrt model."""
+import os
+from os.path import join
+import torch
+
+from common.azure import setup_azure
+from common.config import load_config
+from common.loader import load_tokenized_data, select_patient_subset, save_pids
+from common.setup import setup_run_folder, get_args
+from model.behrt import BertForMaskedLM, BertConfig
+from torch.optim import AdamW
+from data.dataset import MLMDataset
+from trainer.trainer import EHRTrainer
+from transformers import get_linear_schedule_with_warmup
+
+
+args = get_args('pretrain.yaml')
+config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), args.config_path)
+
+
+def main_train(config_path):
+    cfg = load_config(config_path)
+    run = None
+    if cfg.env=='azure':
+        run, mount_context = setup_azure(cfg.paths.run_name)
+        cfg.paths.data_path = join(mount_context.mount_point, cfg.paths.data_path)
+
+    logger = setup_run_folder(cfg)
+    
+    
+    logger.info(f"Loading data from {cfg.paths.data_path} with {cfg.train_data.get('num_patients', 'all')} train patients and {cfg.val_data.get('num_patients', 'all')} val patients")
+    train_features, train_pids, val_features, val_pids, vocabulary = load_tokenized_data(cfg)
+    train_features, train_pids, val_features, val_pids = select_patient_subset(train_features, train_pids, val_features, val_pids, cfg.train_data.num_patients, cfg.val_data.num_patients)
+    train_features = adapt_to_behrt(train_features)
+    val_features = adapt_to_behrt(val_features)
+    run_folder = join(cfg.paths.output_path, cfg.paths.run_name)
+    save_pids(run_folder, train_pids, val_pids)
+    torch.save(vocabulary, join(run_folder, 'vocabulary.pt'))
+    train_dataset = MLMDataset(train_features, vocabulary, **cfg.dataset)
+    val_dataset = MLMDataset(val_features, vocabulary, **cfg.dataset)
+
+    cfg.model.vocab_size = len(train_dataset.vocabulary)
+    logger.info('Initializing model')
+    model = BertForMaskedLM(
+        BertConfig(
+            cfg.model
+        )
+    )
+
+
+    logger.info('Initializing optimizer')
+    optimizer = AdamW(
+        model.parameters(),
+        **cfg.optimizer
+    )
+    if cfg.scheduler:
+        scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=cfg.scheduler.num_warmup_steps, num_training_steps=cfg.scheduler.num_training_steps)
+
+
+    logger.info('Initialize trainer')
+    trainer = EHRTrainer( 
+        model=model, 
+        optimizer=optimizer,
+        scheduler=scheduler,
+        train_dataset=train_dataset, 
+        val_dataset=val_dataset, 
+        args=cfg.trainer_args,
+        metrics=cfg.metrics,
+        cfg=cfg,
+        logger=logger,
+        run=run,
+    )
+    logger.info('Start training')
+    trainer.train()
+    if cfg.env == 'azure':
+        from azure_run import file_dataset_save
+        file_dataset_save(local_path=join('outputs', cfg.paths.run_name), datastore_name = "workspaceblobstore",
+                    remote_path = join("PHAIR", 'models', cfg.paths.type, cfg.paths.run_name))
+        mount_context.stop()
+    logger.info("Done")
+
+def adapt_to_behrt(features):
+    del features['abspos']
+    features['age'] = [convert_to_int(ages) for ages in features['age']]
+    features['position_ids'] = [convert_segment_to_position(segments) for segments in features['segment']]
+    features['segment'] = [convert_segment(segments) for segments in features['segment']]
+    return features
+
+def convert_to_int(ages: list):
+    converted_ages = []
+    for age in ages:
+        if age<0:
+            converted_ages.append(0)
+        else:
+            converted_ages.append(int(age))
+    return converted_ages
+
+def convert_segment(segments: list):
+    converted_segments = []
+    flag = 0
+    for i, segment in enumerate(segments):
+        converted_segments.append(flag)
+        if i < len(segments) - 1:
+            if segment != segments[i+1]:
+                flag = 1 - flag
+    return converted_segments
+
+def convert_segment_to_position(segments: list):
+    converted_segments = []
+    flag = 0
+    for i, segment in enumerate(segments):
+        converted_segments.append(flag)
+        if i < len(segments) - 1:
+            if segment != segments[i+1]:
+                flag += 1
+    return converted_segments
+    
+
+if __name__ == '__main__':
+    main_train(config_path)
---
+++ ehr2vec/model/behrt.py
@@ -0,0 +1,157 @@
+import torch.nn as nn
+import pytorch_pretrained_bert as Bert
+import numpy as np
+import torch
+import os
+from collections import namedtuple
+
+class BertConfig(Bert.modeling.BertConfig):
+    def __init__(self, config):
+        super(BertConfig, self).__init__(
+            vocab_size_or_config_json_file=config.get('vocab_size'),
+            hidden_size=config['hidden_size'],
+            num_hidden_layers=config.get('num_hidden_layers'),
+            num_attention_heads=config.get('num_attention_heads'),
+            intermediate_size=config.get('intermediate_size'),
+            hidden_act=config.get('hidden_act','gelu'),
+            hidden_dropout_prob=config.get('hidden_dropout_prob', 0.1),
+            attention_probs_dropout_prob=config.get('attention_probs_dropout_prob', 0.1),
+            max_position_embeddings = config.get('max_position_embeddings'),
+            initializer_range=config.get('initializer_range', 0.02),
+        )
+        self.seg_vocab_size = 2
+        self.age_vocab_size = 120
+
+    def save_pretrained(self, folder):
+        """ Save a configuration object to the specified directory"""
+        os.makedirs(folder, exist_ok=True)
+        config_file = os.path.join(folder, 'config.json')
+        with open(config_file, 'w') as f:
+            f.write(self.to_json_string())
+        
+class BertEmbeddings(nn.Module):
+    """Construct the embeddings from word, segment, age
+    """
+
+    def __init__(self, config):
+        super(BertEmbeddings, self).__init__()
+        self.word_embeddings = nn.Embedding(config.vocab_size, config.hidden_size)
+        self.segment_embeddings = nn.Embedding(config.seg_vocab_size, config.hidden_size)
+        self.age_embeddings = nn.Embedding(config.age_vocab_size, config.hidden_size)
+        self.posi_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size). \
+            from_pretrained(embeddings=self._init_posi_embedding(config.max_position_embeddings, config.hidden_size))
+
+        self.LayerNorm = Bert.modeling.BertLayerNorm(config.hidden_size, eps=1e-12)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+
+    def forward(self, word_ids, age_ids=None, seg_ids=None, posi_ids=None, age=True):
+        if seg_ids is None:
+            seg_ids = torch.zeros_like(word_ids)
+        if age_ids is None:
+            age_ids = torch.zeros_like(word_ids)
+        if posi_ids is None:
+            posi_ids = torch.zeros_like(word_ids)
+        word_embed = self.word_embeddings(word_ids)
+        segment_embed = self.segment_embeddings(seg_ids)
+        age_embed = self.age_embeddings(age_ids)
+        posi_embeddings = self.posi_embeddings(posi_ids)
+
+        if age:
+            embeddings = word_embed + segment_embed + age_embed + posi_embeddings
+        else:
+            embeddings = word_embed + segment_embed + posi_embeddings
+        embeddings = self.LayerNorm(embeddings)
+        embeddings = self.dropout(embeddings)
+        return embeddings
+
+    def _init_posi_embedding(self, max_position_embedding, hidden_size):
+        def even_code(pos, idx):
+            return np.sin(pos / (10000 ** (2 * idx / hidden_size)))
+
+        def odd_code(pos, idx):
+            return np.cos(pos / (10000 ** (2 * idx / hidden_size)))
+
+        # initialize position embedding table
+        lookup_table = np.zeros((max_position_embedding, hidden_size), dtype=np.float32)
+
+        # reset table parameters with hard encoding
+        # set even dimension
+        for pos in range(max_position_embedding):
+            for idx in np.arange(0, hidden_size, step=2):
+                lookup_table[pos, idx] = even_code(pos, idx)
+        # set odd dimension
+        for pos in range(max_position_embedding):
+            for idx in np.arange(1, hidden_size, step=2):
+                lookup_table[pos, idx] = odd_code(pos, idx)
+
+        return torch.tensor(lookup_table)
+
+
+class BertModel(Bert.modeling.BertPreTrainedModel):
+    def __init__(self, config):
+        super(BertModel, self).__init__(config)
+        self.embeddings = BertEmbeddings(config=config)
+        self.encoder = Bert.modeling.BertEncoder(config=config)
+        self.pooler = Bert.modeling.BertPooler(config)
+        self.apply(self.init_bert_weights)
+
+    def forward(self, batch,
+                output_all_encoded_layers=True):
+        input_ids = batch['concept']
+        age_ids = batch['age']
+        seg_ids = batch['segment']
+        posi_ids = batch['position_ids']
+        attention_mask = batch['attention_mask']
+        if attention_mask is None:
+            attention_mask = torch.ones_like(input_ids)
+        if age_ids is None:
+            age_ids = torch.zeros_like(input_ids)
+        if seg_ids is None:
+            seg_ids = torch.zeros_like(input_ids)
+        if posi_ids is None:
+            posi_ids = torch.zeros_like(input_ids)
+
+        # We create a 3D attention mask from a 2D tensor mask.
+        # Sizes are [batch_size, 1, 1, to_seq_length]
+        # So we can broadcast to [batch_size, num_heads, from_seq_length, to_seq_length]
+        # this attention mask is more simple than the triangular masking of causal attention
+        # used in OpenAI GPT, we just need to prepare the broadcast dimension here.
+        extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)
+
+        # Since attention_mask is 1.0 for positions we want to attend and 0.0 for
+        # masked positions, this operation will create a tensor which is 0.0 for
+        # positions we want to attend and -10000.0 for masked positions.
+        # Since we are adding it to the raw scores before the softmax, this is
+        # effectively the same as removing these entirely.
+        extended_attention_mask = extended_attention_mask.to(dtype=next(self.parameters()).dtype)  # fp16 compatibility
+        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
+
+        embedding_output = self.embeddings(input_ids, age_ids, seg_ids, posi_ids)
+        encoded_layers = self.encoder(embedding_output,
+                                      extended_attention_mask,
+                                      output_all_encoded_layers=output_all_encoded_layers)
+        sequence_output = encoded_layers[-1]
+        pooled_output = self.pooler(sequence_output)
+        if not output_all_encoded_layers:
+            encoded_layers = encoded_layers[-1]
+        return encoded_layers, pooled_output
+
+
+class BertForMaskedLM(Bert.modeling.BertPreTrainedModel):
+    def __init__(self, config):
+        super(BertForMaskedLM, self).__init__(config)
+        self.bert = BertModel(config)
+        self.cls = Bert.modeling.BertOnlyMLMHead(config, self.bert.embeddings.word_embeddings.weight)
+        self.apply(self.init_bert_weights)
+
+    def forward(self, batch):
+        sequence_output, _ = self.bert(batch,
+                                       output_all_encoded_layers=False)
+        prediction_scores = self.cls(sequence_output)
+
+        if batch.get('target', None) is not None:
+            loss_fct = nn.CrossEntropyLoss(ignore_index=-100)
+            masked_lm_loss = loss_fct(prediction_scores.view(-1, self.config.vocab_size), batch['target'].view(-1))
+            return namedtuple('outputs', ['loss', 'logits'])(loss=masked_lm_loss, logits=prediction_scores)
+        else:
+            return prediction_scores

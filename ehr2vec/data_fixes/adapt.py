fea1c2ac1affb3dba312152811ed3eff6c3d418a
Kiril Klein <kikl@di.ku.dk>
2023-10-13 18:43:10 +0200

    Simplify adapt (since segment is the same as position_ids)

--- ehr2vec/data_fixes/adapt.py
+++ ehr2vec/data_fixes/adapt.py
@@ -3,7 +3,7 @@ class BehrtAdapter:
     def adapt_features(self, features: dict):
         del features['abspos']
         features['age'] = [self.convert_to_int(ages) for ages in features['age']]
-        features['position_ids'] = [self.convert_segment_to_position(segments) for segments in features['segment']]
+        features['position_ids'] = features['segment'] # segment is the same as position_ids
         features['segment'] = [self.convert_segment(segments) for segments in features['segment']]
         return features
     @staticmethod
@@ -28,15 +28,4 @@ class BehrtAdapter:
             if i < len(segments) - 1:
                 if segment != segments[i+1]:
                     flag = 1 - flag
-        return converted_segments
-    @staticmethod
-    def convert_segment_to_position(segments: list):
-        """From segment AABBCC to position 001122"""
-        converted_segments = []
-        flag = 0
-        for i, segment in enumerate(segments):
-            converted_segments.append(flag)
-            if i < len(segments) - 1:
-                if segment != segments[i+1]:
-                    flag += 1
         return converted_segments

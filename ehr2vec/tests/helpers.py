64324dcc76f1897eef77732cd3719e2e28b69958
Mikkelfo <mikk528a@gmail.com>
2024-01-10 10:12:13 +0100

    Updated featuremaker + unittests

--- ehr2vec/tests/helpers.py
+++ ehr2vec/tests/helpers.py
@@ -8,3 +8,16 @@ class FeaturesConfig:
     def __contains__(self, item):
         return self.__getattribute__(item)
         
+    def keys(self):
+        return [attr for attr in dir(self) if not attr.startswith('__') and not callable(getattr(self, attr))]
+
+    def __iter__(self):
+        for attr in dir(self):
+            if not attr.startswith('__') and not callable(getattr(self, attr)):
+                yield attr
+
+    def __len__(self):
+        return len(self.keys())
+
+    def __getitem__(self, item):
+        return self.__getattribute__(item)
--- ehr2vec/tests/test_featuremaker.py
+++ ehr2vec/tests/test_featuremaker.py
@@ -12,18 +12,14 @@ class TestFeatureMaker(unittest.TestCase):
         self.concepts = pd.DataFrame({
             'PID': ['1', '2', '3', '1'],
             'CONCEPT': ['DA1', 'DA2', 'MA1', 'DA2'],
-            'TIMESTAMP': pd.to_datetime(['2020-01-02', '2021-03-20', '2022-05-08', '2023-01-02'])
+            'TIMESTAMP': pd.to_datetime(['2020-01-02', '2021-03-20', '2022-05-08', '2023-01-02']),
+            'ADMISSION_ID': ['A', 'B', 'C', 'D']
         })
         self.patients_info = pd.DataFrame({
             'PID': ['1', '2', '3'],
             'BIRTHDATE': pd.to_datetime(['2000-01-02', '2000-03-20', '2000-05-08']),
             'GENDER': ['Male', 'Female', 'Male']
         })
-
-    def test_reordering(self):
-        for key, pos in self.order.items():
-            if key in self.config.keys():
-                self.assertEqual(self.pipeline[pos].id, key)
 
     def test_call(self):
         features, pids = self.feature_maker(self.concepts, self.patients_info)
@@ -40,6 +36,10 @@ class TestFeatureMaker(unittest.TestCase):
         pipeline = self.feature_maker.create_pipeline()
         self.assertIsInstance(pipeline, list)
         self.assertEqual(len(pipeline), len(self.config))
+
+        for key, pos in self.feature_maker.order.items():
+            if key in self.config.keys():
+                self.assertEqual(pipeline[pos].id, key)
 
 if __name__ == '__main__':
     unittest.main()

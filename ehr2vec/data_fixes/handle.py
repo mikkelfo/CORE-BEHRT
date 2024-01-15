fc85b8ab08504949c76dcb1ffc7309ee37b8d564
Mikkelfo <mikk528a@gmail.com>
2024-01-15 13:31:21 +0100

    Updated Handler and deleted drop option (made mandatory) + unittest

--- ehr2vec/data_fixes/handle.py
+++ ehr2vec/data_fixes/handle.py
@@ -1,22 +1,19 @@
 import pandas as pd
 
 
-class Handler():
-    def __init__(self, concept_fill='[UNK]', num_fill=-100, drop=True):
-        self.concept_fill = concept_fill
-        self.num_fill = num_fill
-        self.drop = drop
+class Handler:
+    def __init__(self, min_age: int = -1, max_age: int = 120):
+        self.min_age = min_age
+        self.max_age = max_age
 
-    def __call__(self, features: dict)->dict:
+    def __call__(self, features: dict) -> dict:
         return self.handle(features)
 
-    def handle(self, features: dict)->dict:
+    def handle(self, features: dict) -> dict:
         """Handle the features, including: incorrect ages, nans, and segments."""
         handled_patients = {k: [] for k in features}
-        for i in range(len(features['concept'])):
-            patient = {k: v[i] for k, v in features.items()}
-
-            patient = self.handle_incorrect_ages(patient)
+        for patient in self._iter_patients(features):
+            patient = self.handle_incorrect_ages(patient, min_age=self.min_age, max_age=self.max_age)
 
             patient = self.handle_nans(patient)
             if 'segment' in patient:
@@ -27,30 +24,24 @@ class Handler():
 
         return handled_patients
 
-    def handle_incorrect_ages(self, patient: dict)->dict:
-        correct_indices = [i for i, age in enumerate(patient['age']) if -1 <= age <= 120]
-        if self.drop:
-            for key, values in patient.items():
-                patient[key] = [values[i] for i in correct_indices]
-        else:
-            for key, values in patient.items():
-                patient[key] = [values[i] if i in correct_indices else self.num_fill for i in range(len(values))]
+    @staticmethod
+    def handle_incorrect_ages(patient: dict, min_age: int = -1, max_age: int = 120) -> dict:
+        correct_indices = set([i for i, age in enumerate(patient['age']) if min_age <= age <= max_age])
+        
+        for key, values in patient.items():
+            patient[key] = [values[i] for i in correct_indices]
 
         return patient
 
-    def handle_nans(self, patient: dict)->dict:
-        if self.drop:
-            nan_indices = []
-            for values in patient.values():
-                nan_indices.extend([i for i, v in enumerate(values) if pd.isna(v)])
+    @staticmethod
+    def handle_nans(patient: dict) -> dict:
+        nan_indices = []
+        for values in patient.values():
+            nan_indices.extend([i for i, v in enumerate(values) if pd.isna(v)])
 
-            for key, values in patient.items():
-                patient[key] = [v for i, v in enumerate(values) if i not in set(nan_indices)]
-        
-        else:
-            for key, values in patient.items():
-                filler = self.concept_fill if key == 'concept' else self.num_fill
-                patient[key] = [filler if pd.isna(v) else v for v in values]
+        nan_indices = set(nan_indices)
+        for key, values in patient.items():
+            patient[key] = [v for i, v in enumerate(values) if i not in nan_indices]
 
         return patient
 
@@ -61,4 +52,9 @@ class Handler():
         converter = {k: v for (k,v) in zip(segment_set, correct_segments)}
 
         return [converter[segment] for segment in segments]
+    
+    @staticmethod
+    def _iter_patients(features: dict) -> dict:
+        for i in range(len(features['concept'])):
+            yield {k: v[i] for k, v in features.items()}
 

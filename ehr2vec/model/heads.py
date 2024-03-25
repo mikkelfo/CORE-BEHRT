f727df41e60687bf9583eb030bdc93fcaa55dc35
Mikkelfo <mikk528a@gmail.com>
2024-03-25 13:32:18 +0100

    Implemented more and revamped existing pooling strategies with proper attention_masking

--- ehr2vec/model/heads.py
+++ ehr2vec/model/heads.py
@@ -17,7 +17,7 @@ class MLMHead(torch.nn.Module):
         self.bias = torch.nn.Parameter(torch.zeros(config.vocab_size))
         self.decoder.bias = self.bias
 
-    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+    def forward(self, hidden_states: torch.Tensor, attention_mask = None) -> torch.Tensor:
         x = self.dense(hidden_states)
         x = self.activation(x)
         x = self.LayerNorm(x)
@@ -42,10 +42,13 @@ class AttentionPool(torch.nn.Module):
         self.query = torch.nn.Parameter(torch.randn(hidden_size))
         self.key = torch.nn.Linear(hidden_size, hidden_size)
 
-    def forward(self, x):
+    def forward(self, x, attention_mask=None):
         # Compute attention scores using a dot product between a learnable query and the keys
         keys = self.key(x)  # Shape: [batch_size, seq_length, hidden_size]
         attn_scores = torch.matmul(keys, self.query)  # Shape: [batch_size, seq_length]
+
+        expanded_attention_mask = attention_mask
+        attn_scores[expanded_attention_mask == 0] = 0
 
         # Apply softmax to get attention weights
         attn_weights = torch.nn.functional.softmax(attn_scores, dim=1).unsqueeze(-1)  # Shape: [batch_size, seq_length, 1]
@@ -60,32 +63,48 @@ class FineTuneHead(torch.nn.Module):
         self.classifier = torch.nn.Linear(config.hidden_size, 1)
         if 'extend_head' in config.to_dict():
             self.initialize_extended_head(config)
-        pool_type = config.pool_type
-        if pool_type == 'cls':
+
+        pool_type = config.pool_type.lower()
+        if self.pool_type == 'cls':
             self.pool = self.pool_cls
-        elif pool_type == 'mean':
+        elif self.pool_type == 'mean':
             self.pool = self.pool_mean
-        elif pool_type == 'sum':
+        elif self.pool_type == 'sum':
             self.pool = self.pool_sum
-        elif pool_type == 'attention':
+        elif self.pool_type == 'max':
+            self.pool = self.pool_max
+        elif self.pool_type == 'attention':
             self.pool = AttentionPool(config.hidden_size)
+        elif self.pool_type == 'gru':
+            self.pool = BaseRNN(config, torch.nn.GRU)
+        elif self.pool_type == 'lstm':
+            self.pool = BaseRNN(config, torch.nn.LSTM)
         else:
             logger.warning(f'Unrecognized pool_type: {pool_type}. Defaulting to CLS pooling.')
-            self.pool = self.pool_cls # Default to CLS pooling if pool_type is not recognized
+            self.pool_type = 'cls' # Default to CLS pooling if pool_type is not recognized
+            self.pool = self.pool_cls
 
-    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
-        x = self.pool(hidden_states)
-        x = self.classifier(x)
+    def forward(self, hidden_states: torch.Tensor, attention_mask=None) -> torch.Tensor:
+        x = self.pool(hidden_states, attention_mask=attention_mask)
+        if self.pool_type != 'gru' and self.pool_type != 'lstm':
+            x = self.classifier(x)
         return x
 
-    def pool_cls(self, x):
+    def pool_cls(self, x, attention_mask=None):
         return x[:, 0]
 
-    def pool_mean(self, x):
-        return x.mean(dim=1)
+    def pool_mean(self, x, attention_mask):
+        sum_embeddings = torch.sum(x * attention_mask.unsqueeze(-1), dim=1)
+        sum_mask = attention_mask.sum(dim=1).unsqueeze(-1)
+        return sum_embeddings / sum_mask
 
-    def pool_sum(self, x):
-        return x.sum(dim=1)
+    def pool_sum(self, x, attention_mask):
+        return torch.sum(x * attention_mask.unsqueeze(-1), dim=1)
+    
+    def pool_max(self, x, attention_mask):
+        expanded_attention_mask = attention_mask.unsqueeze(-1).expand(x.shape)
+        x[expanded_attention_mask == 0] = -1e9
+        return torch.max(x, dim=1).values
     
     def initialize_extended_head(self, config):
         if config.extend_head.get('hidden_size', None) is not None:
@@ -97,43 +116,37 @@ class FineTuneHead(torch.nn.Module):
         self.cls_layer = torch.nn.Linear(intermediate_size, 1)
         self.classifier = torch.nn.Sequential(self.hidden_layer, self.activation, self.cls_layer)
 
-class ClassifierGRU(torch.nn.Module):
-    def __init__(self, config, bidirectional=False):
+
+
+class BaseRNN(torch.nn.Module):
+    def __init__(self, config, rnn_type) -> None:
         super().__init__()
         self.hidden_size = config.hidden_size
-        self.bidirectional = bidirectional
-        self.rnn = torch.nn.GRU(self.hidden_size, self.hidden_size, batch_first=True, 
-                                bidirectional=bidirectional)
+        self.bidirectional = config.to_dict().get('bidirectional', False)
+        self.rnn = rnn_type(self.hidden_size, self.hidden_size, batch_first=True, 
+                            bidirectional=self.bidirectional)
         # Adjust the input size of the classifier based on the bidirectionality
-        classifier_input_size = self.hidden_size * 2 if bidirectional else self.hidden_size
+        classifier_input_size = self.hidden_size * 2 if self.bidirectional else self.hidden_size
         self.classifier = torch.nn.Linear(classifier_input_size, 1)
 
-    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
-        # Pass the hidden states through the RNN
-        output, _ = self.rnn(hidden_states)
+    def forward(self, hidden_states: torch.Tensor, attention_mask=None) -> torch.Tensor:
+        lengths = attention_mask.sum(dim=1)
+        packed = torch.nn.utils.rnn.pack_padded_sequence(hidden_states, lengths, batch_first=True, enforce_sorted=False)
+        # Pass the hidden states through the RßNN
+        output, _ = self.rnn(packed)
+        # Unpack it back to a padded sequence
+        output, _ = torch.nn.utils.rnn.pad_packed_sequence(output, batch_first=True)
+        last_sequence_idx = lengths - 1
+        
         # Use the last output of the RNN as input to the classifier
         # If bidirectional, we need to concatenate the last output from the forward
         # pass and the first output from the backward pass
+        forward_output = output[torch.arange(output.shape[0]), last_sequence_idx, :self.hidden_size]  # Last non-padded output from the forward pass
         if self.bidirectional:
-            forward_output = output[:, -1, :self.hidden_size]  # Last output from the forward pass
             backward_output = output[:, 0, self.hidden_size:]  # First output from the backward pass
             x = torch.cat((forward_output, backward_output), dim=-1)
         else:
-            x = output[:, -1, :]  # Last output for unidirectional
-        x = self.classifier(x)
-        return x
-    
-class ClassifierLSTM(torch.nn.Module):
-    def __init__(self, config):
-        super().__init__()
-        self.rnn = torch.nn.LSTM(config.hidden_size, config.hidden_size, batch_first=True)
-        self.classifier = torch.nn.Linear(config.hidden_size, 1)
-
-    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
-        # Pass the hidden states through the RNN
-        output, _ = self.rnn(hidden_states)
-        # Use the last output of the RNN as input to the classifier
-        x = output[:, -1, :]
+            x = forward_output  # Last output for unidirectional
         x = self.classifier(x)
         return x
     
